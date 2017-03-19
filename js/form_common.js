1bd44430f0a8214395b3703bb606880d9983cc2e
Ramon Silva <silva018-mg@yahoo.com.br>
2017-03-19 19:09:13 -0300

    Remoção de arquivos desnecessários

--- js/form_common.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-	
-	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
-	
-	$('select').select2();
-    $('.colorpicker').colorpicker();
-    $('.datepicker').datepicker();
-});
