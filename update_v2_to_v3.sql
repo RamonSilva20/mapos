ef47129d7f9622830ec7c5af8e66eea9e7addae0
Ramon Silva <silva018-mg@yahoo.com.br>
2017-06-29 19:54:41 -0300

    Correções no arquivo de update

--- update_v2_to_v3.sql
+++ update_v2_to_v3.sql
@@ -9,3 +9,9 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
         `data` blob NOT NULL,
         KEY `ci_sessions_timestamp` (`timestamp`)
 );
+
+-- Alterando o tamanho do campo senha
+ALTER TABLE usuarios MODIFY senha VARCHAR(200) NOT NULL;
+
+-- Reset de senha do usuário administrador
+UPDATE usuarios SET senha = '94556715d7862d57e603e5e7389e0174227388d94090370517e3cfe5b1cccfbf3647bacd8dfc6190492c42d19e76df96308236c87c83ff78c37c01678d675e4fZE8TIK5YP2vt2j7+3ta7mfbOgY8wdMfs/vPCG5YBWh4=' WHERE idUsuarios = 1
