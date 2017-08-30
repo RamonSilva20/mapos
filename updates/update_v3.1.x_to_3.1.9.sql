f25d803ec21bb1add00c63e2be1bbf7ed55c8019
Ramon Silva <RamonSilva20@users.noreply.github.com>
2017-08-29 23:36:35 -0300

    Adição de valor default para o campo baixado na tabela de lançamentos

--- updates/update_v3.1.x_to_3.1.9.sql
+++ updates/update_v3.1.x_to_3.1.9.sql
@@ -1,3 +1,5 @@
 -- Alterando tabela de produtos
 
 ALTER TABLE `produtos` ADD `saida` TINYINT(1) NULL DEFAULT NULL AFTER `estoqueMinimo`, ADD `entrada` TINYINT(1) NULL DEFAULT NULL AFTER `saida`;
+
+ALTER TABLE `lancamentos` CHANGE `baixado` `baixado` TINYINT(1) NULL DEFAULT '0';
