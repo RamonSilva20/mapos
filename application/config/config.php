c3fb5e8a29250494b0c47087345ee1710dc84fb0
Ramon Silva <RamonSilva20@users.noreply.github.com>
2017-09-10 18:51:19 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -4,7 +4,7 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 
-$config['app_version'] = '3.1.10'; 
+$config['app_version'] = '3.1.11'; 
 
 /*
 |--------------------------------------------------------------------------
