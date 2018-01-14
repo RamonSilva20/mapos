c2db047355d684fe891d7c929696c38a088653d1
Ramon Silva <RamonSilva20@users.noreply.github.com>
2018-01-13 23:23:09 -0200

    Adicionando tabela de sessão - arquivo banco.sql

--- docs/banco.sql
+++ docs/banco.sql
@@ -18,6 +18,17 @@ CREATE SCHEMA IF NOT EXISTS `mapos` DEFAULT CHARACTER SET utf8 ;
 USE `mapos` ;
 
 -- -----------------------------------------------------
+-- Table `ci_sessions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+        `id` varchar(128) NOT NULL,
+        `ip_address` varchar(45) NOT NULL,
+        `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        `data` blob NOT NULL,
+        KEY `ci_sessions_timestamp` (`timestamp`)
+);
+
+-- -----------------------------------------------------
 -- Table `groups`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `groups` ;
