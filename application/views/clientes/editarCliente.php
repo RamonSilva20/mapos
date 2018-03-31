289fb2110465b90f71253d5e8ef847b6bf241302
Henrique Miranda <lhmiranda29@gmail.com>
2018-03-31 17:31:18 -0300

    Add busca de endereço pelo CEP
    Adicionando busca de endereço pelo CEP em editarCliente.php

--- application/views/clientes/editarCliente.php
+++ application/views/clientes/editarCliente.php
@@ -45,18 +45,25 @@
                             <input id="email" type="text" name="email" value="<?php echo $result->email; ?>"  />
                         </div>
                     </div>
-
+                    
                     <div class="control-group" class="control-label">
-                        <label for="rua" class="control-label">Rua<span class="required">*</span></label>
+                        <label for="cep" class="control-label">CEP<span class="required">*</span></label>
                         <div class="controls">
-                            <input id="rua" type="text" name="rua" value="<?php echo $result->rua; ?>"  />
+                            <input id="cep" type="text" name="cep" value="<?php echo $result->cep; ?>"  />
                         </div>
                     </div>
-
+                    
                     <div class="control-group">
                         <label for="numero" class="control-label">Número<span class="required">*</span></label>
                         <div class="controls">
                             <input id="numero" type="text" name="numero" value="<?php echo $result->numero; ?>"  />
+                        </div>
+                    </div>
+                    
+                    <div class="control-group" class="control-label">
+                        <label for="rua" class="control-label">Rua<span class="required">*</span></label>
+                        <div class="controls">
+                            <input id="rua" type="text" name="rua" value="<?php echo $result->rua; ?>"  />
                         </div>
                     </div>
 
@@ -81,12 +88,7 @@
                         </div>
                     </div>
 
-                    <div class="control-group" class="control-label">
-                        <label for="cep" class="control-label">CEP<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="cep" type="text" name="cep" value="<?php echo $result->cep; ?>"  />
-                        </div>
-                    </div>
+                    
 
 
 
@@ -149,3 +151,68 @@
       });
 </script>
 
+<script type="text/javascript" >
+
+        $(document).ready(function() {
+
+            function limpa_formulario_cep() {
+                // Limpa valores do formulário de cep.
+                $("#rua").val("");
+                $("#bairro").val("");
+                $("#cidade").val("");
+                $("#estado").val("");
+            }
+            
+            //Quando o campo cep perde o foco.
+            $("#cep").blur(function() {
+
+                //Nova variável "cep" somente com dígitos.
+                var cep = $(this).val().replace(/\D/g, '');
+
+                //Verifica se campo cep possui valor informado.
+                if (cep != "") {
+
+                    //Expressão regular para validar o CEP.
+                    var validacep = /^[0-9]{8}$/;
+
+                    //Valida o formato do CEP.
+                    if(validacep.test(cep)) {
+
+                        //Preenche os campos com "..." enquanto consulta webservice.
+                        $("#rua").val("...");
+                        $("#bairro").val("...");
+                        $("#cidade").val("...");
+                        $("#estado").val("...");
+
+                        //Consulta o webservice viacep.com.br/
+                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#rua").val(dados.logradouro);
+                                $("#bairro").val(dados.bairro);
+                                $("#cidade").val(dados.localidade);
+                                $("#estado").val(dados.uf);
+                                document.getElementById("numero").focus();
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulario_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                    } //end if.
+                    else {
+                        //cep é inválido.
+                        limpa_formulario_cep();
+                        alert("Formato de CEP inválido.");
+                    }
+                } //end if.
+                else {
+                    //cep sem valor, limpa formulário.
+                    limpa_formulario_cep();
+                }
+            });
+        });
+
+    </script>
