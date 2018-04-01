b9d0ecc4d0bdac23b19475239875f24c123ae8a7
Henrique Miranda <lhmiranda29@gmail.com>
2018-04-01 10:23:41 -0300

    Corrigindo BUG que não exclui anexo da OS
    De: var url = '<?php echo base_url(); ?>os/excluirAnexo/'; 
    Para: var url = '<?php echo base_url(); ?>index.php/os/excluirAnexo/';

--- application/views/os/editarOs.php
+++ application/views/os/editarOs.php
@@ -698,7 +698,7 @@ $(document).ready(function(){
            event.preventDefault();
            var link = $(this).attr('link');
            var id = $(this).attr('imagem');
-           var url = '<?php echo base_url(); ?>os/excluirAnexo/';
+           var url = '<?php echo base_url(); ?>index.php/os/excluirAnexo/';
            $("#div-visualizar-anexo").html('<img src="'+link+'" alt="">');
            $("#excluir-anexo").attr('link', url+id);
 
