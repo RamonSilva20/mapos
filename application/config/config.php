782c38e9103fd55ac1dc289eb572254aaf822243
Ramon Silva <silva018-mg@yahoo.com.br>
2018-11-24 21:34:33 -0200

    Adicionando suporte a HMVC

--- application/config/config.php
+++ application/config/config.php
@@ -565,3 +565,8 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
+$config['modules_locations'] = array(
+    APPPATH.'modules/' => '../modules/',
+);
