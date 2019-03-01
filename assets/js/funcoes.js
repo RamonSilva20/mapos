f1e0277ee418d18e99f704634551da7d0c1f882b
Fábio Barbosa <fabiobarbosa@gmx.com>
2019-03-01 10:03:35 -0300

    Meloria na funcao de aplicacao da mascara

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,9 +1,28 @@
-$(function() {
-			$("#cpf").mask("000.000.000-00")
-			$("#cnpj").mask("000.000.000/0000-00")
-			$("#rg").mask("00.000.000-0")
-			$("#celular").mask("(00)00000-0000")
-			$("#telefone").mask("(00)0000-0000")
-			$("#cep").mask("00000-000")
+$(function () {
+
+	$("#rg").mask("00.000.000-0")
+	$("#celular").mask("(00)00000-0000")
+	$("#telefone").mask("(00)0000-0000")
+	$("#cep").mask("00000-000")
+});
+$(function formatar(mascara, documento) {
+	var i = documento.value.length;
+	var saida = mascara.substring(0, 1);
+	var texto = mascara.substring(i)
+	if (texto.substring(0, 1) != saida) {
+		documento.value += texto.substring(0, 1);
+	}
+});
+
+$(function () {
+	var options = {
+		onKeyPress: function (cpfcnpj, e, field, options) {
+			var masks = ['000.000.000-000', '000.000.000/0000-00'];
+			var mask = (cpfcnpj.length > 14) ? masks[1] : masks[0];
+			$('.cpfcnpj').mask(mask, options);
 		}
-); 
+	};
+
+	$('.cpfcnpj').mask('000.000.000-000', options);
+
+});
