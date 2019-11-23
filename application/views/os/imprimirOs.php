2358fdf2cf8c2dd87c02035a1dfdcb694f8768e6
Ramon Silva <silva018-mg@yahoo.com.br>
2019-11-22 22:06:57 -0200

    Removendo duplicidade de cabeçalho

--- application/views/os/imprimirOs.php
+++ application/views/os/imprimirOs.php
@@ -21,63 +21,6 @@ $totalProdutos = 0; ?>
 
 <body>
 
-  <div class="container-fluid">
-    <div class="row-fluid">
-      <div class="span12">
-       
-        <div class="invoice-content">
-                <div class="invoice-head" style="margin-bottom: 0">
-
-                    <table class="table table-condensed">
-                        <tbody>
-                            <?php if ($emitente == null) {?>
-                                        
-                            <tr>
-                                <td colspan="3" class="alert">Você precisa configurar os dados do emitente. >>><a href="<?php echo base_url(); ?>index.php/mapos/emitente">Configurar</a><<<</td>
-                            </tr>
-                            <?php } else {?>
-                            <tr>
-                                <td style="width: 25%"><img src=" <?php echo base_url().$emitente[0]->url_logo; ?> " style="max-height: 100px"></td>
-                                <td> <span style="font-size: 20px; "> <?php echo $emitente[0]->nome; ?></span> </br><span><?php echo $emitente[0]->cnpj; ?> </br> <?php echo $emitente[0]->rua.', '.$emitente[0]->numero.' - '.$emitente[0]->bairro.' - '.$emitente[0]->cidade.' - '.$emitente[0]->uf; ?> </span> </br> <span> E-mail: <?php echo $emitente[0]->email.' - Fone: '.$emitente[0]->telefone; ?></span></td>
-                                <td style="width: 18%; text-align: center"><b>N° OS:</b> <span ><?php echo $result->idOs?></span></br> </br> <span>Emissão: <?php echo date('d/m/Y')?></span></td>
-                            </tr>
-
-                            <?php } ?>
-                        </tbody>
-                    </table>
-
-            
-                    <table class="table table-condensend">
-                        <tbody>
-                            <tr>
-                                <td style="width: 50%; padding-left: 0">
-                                    <ul>
-                                        <li>
-                                            <span><h5><b>CLIENTE</b></h5>
-                                            <span><?php echo $result->nomeCliente?></span><br/>
-                                            <span><?php echo $result->rua?>, <?php echo $result->numero?>, <?php echo $result->bairro?></span>, 
-                                            <span><?php echo $result->cidade?> - <?php echo $result->estado?></span><br>
-                                            <span>E-mail: <?php echo $result->email?></span><br>
-                                            <span>Celular: <?php echo $result->celular_cliente?></span>
-                                        </li>
-                                    </ul>
-                                </td>
-                                <td style="width: 50%; padding-left: 0">
-                                    <ul>
-                                        <li>
-                                            <span><h5><b>RESPONSÁVEL</b></h5></span>
-                                            <span><?php echo $result->nome?></span> <br/>
-                                            <span>Telefone: <?php echo $result->telefone_usuario?></span><br/>
-                                            <span>Email: <?php echo $result->email_responsavel ?></span>
-                                        </li>
-                                    </ul>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table> 
-    
-                </div>
-
     <div class="container-fluid">
         <div class="row-fluid">
             <div class="span12">
