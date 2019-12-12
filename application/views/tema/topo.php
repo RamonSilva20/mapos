1a9ab4276168ace627bde6ea5b04409ab993c363
ZanzouShio <36826312+ZanzouShio@users.noreply.github.com>
2019-12-12 07:12:54 -0300

    Update topo.php

--- application/views/tema/topo.php
+++ application/views/tema/topo.php
@@ -74,7 +74,7 @@
           setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
           date_default_timezone_set('America/Sao_Paulo');
           $uppercaseMonth = ucfirst(gmstrftime('%B'));
-          echo ucfirst(strftime('%A, %d de ' . $uppercaseMonth . ' de %Y', strtotime('today')));
+          echo utf8_encode(ucfirst(strftime( '%A, %d de ' .$uppercaseMonth. ' de %Y', strtotime('today'))));
           ?>
           <i class="far fa-clock"></i></i> <span id="timer"></span></span></li>
     </ul>
