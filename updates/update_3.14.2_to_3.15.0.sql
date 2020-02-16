75076bc45414e3e04c1422bb3c863faf006c3ed4
Wilmerson <will.phelipe@gmail.com>
2020-02-16 16:53:56 -0300

    Update update_3.14.2_to_3.15.0.sql

--- updates/update_3.14.2_to_3.15.0.sql
+++ updates/update_3.14.2_to_3.15.0.sql
@@ -6,14 +6,3 @@ INSERT INTO `configuracoes` (`idConfig`, `config`, `valor`) VALUES
 (4, 'per_page', '10'),
 (5, 'os_notification', 'cliente'),
 (6, 'control_estoque', '1');
-
-CREATE TABLE `pagamento` (
-  `idPag` INT NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `client_id` varchar(200) NOT NULL,
-  `client_secret` varchar(200) NOT NULL,
-  `public_key` varchar(200) NOT NULL,
-  `access_token` varchar(200) NOT NULL,
-  `default_pag` int(1) NOT NULL,
-   PRIMARY KEY (`idPag`)
-);
