dffc2da9796f042b5b0378053a35a4045463dabc
Bremyarck <44008062+Bremyarck@users.noreply.github.com>
2020-02-17 16:56:40 -0300

    Update constants.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,33 +1,35 @@
-<?php  if (! defined('BASEPATH')) {
+<?php
+
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
 /*
-|--------------------------------------------------------------------------
-| File and Directory Modes
-|--------------------------------------------------------------------------
-|
-| These prefs are used when checking and setting modes when working
-| with the file system.  The defaults are fine on servers with proper
-| security, but you may wish (or even need) to change the values in
-| certain environments (Apache running a separate process for each
-| user, PHP under CGI with Apache suEXEC, etc.).  Octal values should
-| always be used to set the mode correctly.
-|
-*/
+  |--------------------------------------------------------------------------
+  | File and Directory Modes
+  |--------------------------------------------------------------------------
+  |
+  | These prefs are used when checking and setting modes when working
+  | with the file system.  The defaults are fine on servers with proper
+  | security, but you may wish (or even need) to change the values in
+  | certain environments (Apache running a separate process for each
+  | user, PHP under CGI with Apache suEXEC, etc.).  Octal values should
+  | always be used to set the mode correctly.
+  |
+ */
 define('FILE_READ_MODE', 0644);
 define('FILE_WRITE_MODE', 0666);
 define('DIR_READ_MODE', 0755);
 define('DIR_WRITE_MODE', 0777);
 
 /*
-|--------------------------------------------------------------------------
-| File Stream Modes
-|--------------------------------------------------------------------------
-|
-| These modes are used when working with fopen()/popen()
-|
-*/
+  |--------------------------------------------------------------------------
+  | File Stream Modes
+  |--------------------------------------------------------------------------
+  |
+  | These modes are used when working with fopen()/popen()
+  |
+ */
 
 define('FOPEN_READ', 'rb');
 define('FOPEN_READ_WRITE', 'r+b');
