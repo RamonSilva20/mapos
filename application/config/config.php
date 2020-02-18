40bad366bc2dc97e2037ba4073693d62a263d31e
Wilmerson <will.phelipe@gmail.com>
2020-02-18 12:56:23 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -152,7 +152,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FCPATH . 'vendor/autoload.php';
+$config['composer_autoload'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
