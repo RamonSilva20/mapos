c75617164d242f96857bf0ccee2aee44fc15686d
Bremyarck <44008062+Bremyarck@users.noreply.github.com>
2020-02-19 21:12:08 -0300

    Pequenas Melhorias
    Mundança para o tema padrão escuro.
    Renomeado o nome do sistema para caracteres maiúsculos.

--- banco.sql
+++ banco.sql
@@ -538,8 +538,8 @@ CREATE TABLE `anotacoes_os` (
 CREATE TABLE `configuracoes` ( `idConfig` INT NOT NULL AUTO_INCREMENT , `config` VARCHAR(20) NOT NULL UNIQUE, `valor` VARCHAR(20) NOT NULL , PRIMARY KEY (`idConfig`)) ENGINE = InnoDB;
 
 INSERT INTO `configuracoes` (`idConfig`, `config`, `valor`) VALUES
-(2, 'app_name', 'Map-OS'),
-(3, 'app_theme', 'white'),
+(2, 'app_name', 'MAPOS'),
+(3, 'app_theme', 'black'),
 (4, 'per_page', '10'),
 (5, 'os_notification', 'cliente'),
 (6, 'control_estoque', '1');
