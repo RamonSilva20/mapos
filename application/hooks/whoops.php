3d86fcc802dbf14b24e0c4a84ef77b2ae0ccaa64
Gianluca Bine <gian_bine@hotmail.com>
2020-03-28 13:09:26 -0300

    Implementado hook para capturar erros da aplicação com whoops

---
+++ application/hooks/whoops.php
@@ -0,0 +1,10 @@
+<?php
+
+class WhoopsHook {
+    public function bootWhoops()
+    {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+        $whoops->register();
+    }
+}
