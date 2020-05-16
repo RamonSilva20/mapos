3955025e8b9fd67aa65b7e8ff077b5e51f322fc5
giampaulo <48699495+giampaulo@users.noreply.github.com>
2020-05-16 10:51:29 -0300

    file manager
    adicionado responsive file manager como gerenciador de arquivos do mapos

---
+++ application/views/arquivos/Old_files/arquivos_BKP.php
@@ -0,0 +1,128 @@
+<link rel="stylesheet" href="<?= base_url(); ?>assets/js/jquery-ui/css/smoothness/jquery-ui-1.9.2.custom.css" />
+<script type="text/javascript" src="<?= base_url() ?>assets/js/jquery-ui/js/jquery-ui-1.9.2.custom.js"></script>
+
+<div class="span12" style="margin-left: 0">
+    <form method="get" action="<?= current_url(); ?>">
+        <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'aArquivo')) : ?>
+            <div class="span3">
+                <a href="<?= base_url(); ?>index.php/arquivos/adicionar" class="btn btn-success span12">
+                    <i class="fas fa-plus"></i>
+                    Adicionar Arquivo
+                </a>
+            </div>
+        <?php endif ?>
+
+        <div class="span5">
+            <input type="text" name="pesquisa" id="pesquisa" placeholder="Digite o nome do documento para pesquisar" class="span12" value="<?= $this->input->get('pesquisa') ?>">
+        </div>
+        <div class="span3">
+            <input type="text" name="data" id="data" placeholder="Data de" class="span6 datepicker" value="<?= $this->input->get('data') ?>">
+            <input type="text" name="data2" id="data2" placeholder="Data até" class="span6 datepicker" value="<?= $this->input->get('data2') ?>">
+        </div>
+        <div class="span1">
+            <button class="span12 btn"> <i class="fas fa-search"></i> </button>
+        </div>
+    </form>
+</div>
+
+<div class="span12" style="margin-left: 0">
+    <div class="widget-box">
+        <div class="widget-title">
+            <span class="icon">
+                <i class="fas fa-hdd"></i>
+            </span>
+            <h5>Arquivos</h5>
+        </div>
+        <div class="widget-content nopadding">
+            <table width="100%" class="table table-bordered ">
+                <thead>
+                    <tr>
+                        <th width="5%">#</th>
+                        <th width="10%">Miniatura</th>
+                        <th width="10%">Nome</th>
+                        <th width="8%">Data</th>
+                        <th>Descrição</th>
+                        <th width="8%">Tamanho</th>
+                        <th width="5%">Extensão</th>
+                        <th width="14%">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+
+                    if (!$results) {
+                        echo '<tr>
+                                <td colspan="5">Nenhum Arquivo Encontrado</td>
+                            </tr>';
+                    }
+                    foreach ($results as $r) : ?>
+                        <tr>
+                            <td><?= $r->idDocumentos ?></td>
+                            <td>
+                                <?php if (getimagesize($r->path)): ?>
+                                    <a href="<?= $r->url ?>"> <img src="<?= $r->url ?> "></a>
+                                <?php else: ?>
+                                    <span>-</span>
+                                <?php endif ?>
+                            </td>
+                            <td><?= $r->documento ?></td>
+                            <td><?= date('d/m/Y', strtotime($r->cadastro)) ?></td>
+                            <td><?= $r->descricao ?></td>
+                            <td><?= $r->tamanho ?> KB</td>
+                            <td><?= $r->tipo ?></td>
+                            <td><?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vArquivo')) : ?>
+                                    <a href="<?= base_url() ?>index.php/arquivos/download/<?= $r->idDocumentos; ?>" class="btn tip-top" style="margin-right: 1%" title="Download">
+                                        <i class="fas fa-download"></i>
+                                    </a>
+                                <?php endif ?>
+
+                                <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'eArquivo')) : ?>
+                                    <a href="<?= base_url() ?>index.php/arquivos/editar/<?= $r->idDocumentos ?>" class="btn btn-info tip-top" style="margin-right: 1%" title="Editar">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                <?php endif ?>
+
+                                <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'dArquivo')) : ?>
+                                    <a href="#modal-excluir" style="margin-right: 1%" role="button" data-toggle="modal" arquivo="<?= $r->idDocumentos ?>" class="btn btn-danger tip-top" title="Excluir Arquivo">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </a>
+                                <?php endif ?>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?= $this->pagination->create_links() ?>
+
+<div id="modal-excluir" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <form action="<?= base_url() ?>index.php/arquivos/excluir" method="post">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            <h5 id="myModalLabel">Excluir Arquivo</h5>
+        </div>
+        <div class="modal-body">
+            <input type="hidden" id="idDocumento" name="id" value="" />
+            <h5 style="text-align: center">Deseja realmente excluir este arquivo?</h5>
+        </div>
+        <div class="modal-footer">
+            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
+            <button class="btn btn-danger">Excluir</button>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(document).on('click', 'a', function(event) {
+            var arquivo = $(this).attr('arquivo');
+            $('#idDocumento').val(arquivo);
+        });
+        $(".datepicker").datepicker({
+            dateFormat: 'dd/mm/yy'
+        });
+    });
+</script>
---
+++ application/views/arquivos/README.md
@@ -0,0 +1,80 @@
+# Responsive FileManager for TinyMCE and CKEditor
+
+[![Build Status](https://travis-ci.org/trippo/ResponsiveFilemanager.svg?branch=master)](https://travis-ci.org/trippo/ResponsiveFilemanager)
+
+Responsive FileManager is a free open-source file manager made with the jQuery library, CSS3, PHP and HTML5 that offers a nice and elegant way to upload and insert files, images and videos. You can use it as external plugin for TinyMCE version 4.x. or CKEditor and you can also use it as a stand-alone file manager to manage and select files. The script automatically creates thumbnails of images for the preview list and can create also external thumbnails to use in your cms or site. It can be configured for automatic resizing of uploaded images or to automatically limit the size. You can personalize the configuration for each folder. You can set a subfolder as the root and change the configuration for each user, page or FileManager call. Is compatible with multi-user mode and you can edit images with aviary editor, sorting files.
+
+DEMO AND DOCUMENTATION: http://www.responsivefilemanager.com/
+
+PRODUCTION VERSION DOWNLOAD: https://github.com/trippo/ResponsiveFilemanager/releases
+
+Released under Creative Commons Attribution-NonCommercial 3.0 Unported License.
+
+Creator : info@albertoperipolli.com - tr1pp0
+
+### Localization
+
+- AZE [Elshad Agayev] 
+- BGR [Stanislav Panev]
+- BRA [paulomanrique]
+- CAT [Manel Peña]
+- CHN [Vu Doan Thang]
+- CRO 
+- CZE [jlusticky]
+- DAN [Morten Hesselberg Grove]
+- ENG
+- ESP [Roberto Santamaria] 
+- FRA [Mathieu Ducharme]
+- GER [Oliver Beta]
+- GRC [vkouvelis]
+- Hebrew [sagie212]
+- HUN [Novak Szabolcs]
+- IND [urayogi]
+- ITA
+- JPN [Vu Doan Thang]
+- LTU [Tomas Norkūnas]
+- MON [Tumenzul Batjargal]
+- NLD [Martijn van der Made]
+- NOR [Pål Schroeder]
+- Persian [web2web esf ir]
+- POL [Michell Hoduń]
+- POR [Sérgio Lima]
+- RUS [vasromand] 
+- SLO [Roman Šovčík]
+- SVN [Peter Benko]
+- SWE [Jon Sten]
+- TUR [Ahmed Faruk Bora]
+- UKR [Sergey]
+
+### Development
+
+In order to work on assets, you need to install [Node.js](http://nodejs.org) and [yarn](https://yarnpkg.com), then cd to your folder and run these commands:
+
+1. Install node packages according to package.js (directory node_modules)
+
+   ```
+   yarn install
+   ```
+3. Compile assets
+
+   ```
+   yarn build
+   ```
+
+Gulp use [laravel-elixir](http://laravel.com/docs/5.0/elixir)
+
+### Credits
+
+- [Bootstrap](http://twitter.github.io/bootstrap)
+- [Bootstrap Lightbox](http://jbutz.github.io/bootstrap-lightbox)
+- [Dropzonejs](http://www.dropzonejs.com)
+- [Fancybox](http://fancybox.net)
+- [TouchSwipe](http://labs.rampinteractive.co.uk/touchSwipe/demos)
+- [PHP Image Magician](http://phpimagemagician.jarrodoberto.com)
+- [Mini icons](http://www.fatcow.com/free-icons)
+- [Jupload](http://jupload.sourceforge.net)
+- [Bootbox](http://bootboxjs.com)
+- [jQuery contextMenu](https://swisnl.github.io/jQuery-contextMenu/)
+- [Bootstrap-modal](https://github.com/jschr/bootstrap-modal)
+- [jPlayer](http://jplayer.org)
+- [Lazy Load Plugin for jQuery](http://www.appelsiini.net/projects/lazyload)
