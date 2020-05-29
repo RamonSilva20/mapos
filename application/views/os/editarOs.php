33553a99c1fe4687b01a5cc03675954cd5133f49
Gianluca Bine <gian_bine@hotmail.com>
2020-05-29 20:42:48 -0300

    Fix/attachments name and delete (#789)
    * Adequação para mostrar nome abaixo do ícone do anexo em OS
    
    * Documentação

--- application/views/os/editarOs.php
+++ application/views/os/editarOs.php
@@ -290,7 +290,12 @@
                                             $thumb = $a->url . '/thumbs/' . $a->thumb;
                                             $link = $a->url .'/'. $a->anexo;
                                         }
-                                        echo '<div class="span3" style="min-height: 150px; margin-left: 0"><a style="min-height: 150px;" href="#modal-anexo" imagem="' . $a->idAnexos . '" link="' . $link . '" role="button" class="btn anexo span12" data-toggle="modal"><img src="' . $thumb . '" alt=""></a></div>';
+                                        echo '<div class="span3" style="min-height: 150px; margin-left: 0">
+                                                <a style="min-height: 150px;" href="#modal-anexo" imagem="' . $a->idAnexos . '" link="' . $link . '" role="button" class="btn anexo span12" data-toggle="modal">
+                                                    <img src="' . $thumb . '" alt="">
+                                                </a>
+                                                <span>'. $a->anexo .'</span>
+                                            </div>';
                                     } ?>
                                 </div>
                             </div>
