08efeb487f0f43fc83a9abedcab4948db34c9b5c
tonnybarros <68646516+tonnybarros@users.noreply.github.com>
2020-08-07 18:54:48 -0300

    Git_user
    Git_user

--- application/config/github_updater.php
+++ application/config/github_updater.php
@@ -5,7 +5,7 @@
 /**
 * The user name of the git hub user who owns the repo
 */
-$config['github_user'] = 'RamonSilva20';
+$config['github_user'] = 'tonnybarros';
 
 /**
 * The repo on GitHub we will be updating from
@@ -23,7 +23,7 @@ $config['github_branch'] = 'master';
 * NOTE: You should only need to set this initially it will be
 * automatically set by the library after subsequent updates.
 */
-$config['current_commit'] = 'ea289a693515f7c01b258dca54d19d9ba8ca50de';
+$config['current_commit'] = '82b546452a3363f3eae253ee0b8393eab4103cb7';
 
 /**
 * A list of files or folders to never perform an update on.
