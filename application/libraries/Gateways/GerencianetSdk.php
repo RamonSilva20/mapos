0f4197b21571b96c0a6a1741509aa54c3b50dafb
Wilmerson <will.phelipe@gmail.com>
2020-12-28 21:16:57 -0300

    Bug Gerar Boleto (#1096)
    Corrigido bug onde so gerava boleto de cliente com CPF, dando erro ao gerar boleto de CNPJ.
    
    Adicionado alguma validações para evitar campo null, string vazia e valor menor igual a zero.

--- application/libraries/Gateways/GerencianetSdk.php
+++ application/libraries/Gateways/GerencianetSdk.php
@@ -10,7 +10,7 @@ class GerencianetSdk
         $client_Secret,
         $nameClient,
         $emailClient,
-        $cpfClient,
+        $cpfCnpjClient,
         $phoneClient,
         $addressRua,
         $addressNumero,
@@ -23,50 +23,79 @@ class GerencianetSdk
         $unit_price,
         $quantity
     ) {
+        try {
+            if ($phoneClient != "" || $phoneClient != null) {
+                $phoneClient = preg_replace("/[^0-9]/", "",  $phoneClient);
+            } else {
+                throw new Exception('O celular do cliente precisa ser preenchido.');
+            }
+            if ($addressCep != "" || $addressCep != null) {
+                $addressCep = preg_replace("/[^0-9]/", "", $addressCep);
+            } else {
+                throw new Exception('O CEP do cliente precisa ser preenchido.');
+            }
+            if ($cpfCnpjClient != "" || $cpfCnpjClient != null) {
+                $cpfCnpjClient = preg_replace("/[^0-9]/", "", $cpfCnpjClient);
+            } else {
+                throw new Exception('O CPF/CNPJ do cliente precisa ser preenchido.');
+            }
+            if ($unit_price != "" && $unit_price != null && intval($unit_price) > 0) {
+                $new_unit_price = intval(preg_replace('/[^0-9]/', '', number_format($unit_price, 2, '.', '')));
+            } else {
+                throw new Exception('Não é possivel gerar pagamento com o valor R$  0,00.');
+            }
 
-        $phoneClient = preg_replace("/[^0-9]/", "", $phoneClient);
-        $addressCep = preg_replace("/[^0-9]/", "", $addressCep);
+            $clientId = $client_Id; // insira seu Client_Id, conforme o ambiente (Des ou Prod)
+            $clientSecret = $client_Secret; // insira seu Client_Secret, conforme o ambiente (Des ou Prod)
 
-        $cpfClient = preg_replace("/[^0-9]/", "", $cpfClient);
+            $options = [
+                'client_id' => $clientId,
+                'client_secret' => $clientSecret,
+                'sandbox' => true // altere conforme o ambiente (true = desenvolvimento e false = producao)
+            ];
 
-        $new_unit_price = intval(preg_replace('/[^0-9]/', '', number_format($unit_price, 2, '.', '')));
+            $item_1 = [
+                'name' => $title . ' ' . $id, // nome do item, produto ou serviço
+                'amount' => $quantity, // quantidade
+                'value' => $new_unit_price // valor (1000 = R$ 10,00) (Obs: É possível a criação de itens com valores negativos. Porém, o valor total da fatura deve ser superior ao valor mínimo para geração de transações.)
+            ];
+            $items = [
+                $item_1
+            ];
+            $metadata = array('notification_url' => 'http://mapos.com.br/'); //Url de notificações
+            $address = [
+                "street" => $addressRua,
+                "number" => $addressNumero,
+                "neighborhood" => $addressBairro,
+                "zipcode" => $addressCep,
+                "city" => $addressCidade,
+                "complement" => '',
+                "state" => $addressEstado,
+            ];
 
-        $clientId = $client_Id; // insira seu Client_Id, conforme o ambiente (Des ou Prod)
-        $clientSecret = $client_Secret; // insira seu Client_Secret, conforme o ambiente (Des ou Prod)
+            if (strlen($cpfCnpjClient) == 11) {
+                $customer = [
+                    'name' => $nameClient, // nome do cliente
+                    'cpf' => $cpfCnpjClient, // cpf válido do cliente
+                    'phone_number' => $phoneClient, // telefone do cliente
+                    'email' => $emailClient,
+                    'address' => $address,
+                ];
+            } else {
+                $juridical_person = [
+                    "corporate_name" => $nameClient, // nome do cliente
+                    "cnpj" => $cpfCnpjClient, // cnpj válido do cliente
+                ];
+                $customer = [
+                    "juridical_person" => $juridical_person,
+                    'phone_number' => $phoneClient, // telefone do cliente
+                    'email' => $emailClient,
+                    'address' => $address,
+                ];
+            }
 
-        $options = [
-            'client_id' => $clientId,
-            'client_secret' => $clientSecret,
-            'sandbox' => true // altere conforme o ambiente (true = desenvolvimento e false = producao)
-        ];
 
-        $item_1 = [
-            'name' => $title . ' ' . $id, // nome do item, produto ou serviço
-            'amount' => $quantity, // quantidade
-            'value' => $new_unit_price // valor (1000 = R$ 10,00) (Obs: É possível a criação de itens com valores negativos. Porém, o valor total da fatura deve ser superior ao valor mínimo para geração de transações.)
-        ];
-        $items = [
-            $item_1
-        ];
-        $metadata = array('notification_url' => 'http://mapos.com.br/'); //Url de notificações
-        $address = [
-            "street" => $addressRua,
-            "number" => $addressNumero,
-            "neighborhood" => $addressBairro,
-            "zipcode" => $addressCep,
-            "city" => $addressCidade,
-            "complement" => '',
-            "state" => $addressEstado,
-        ];
-        $customer = [
-            'name' => $nameClient, // nome do cliente
-            'cpf' => $cpfClient, // cpf válido do cliente
-            'phone_number' => $phoneClient, // telefone do cliente
-            'email' => $emailClient,
-            'address' => $address,
-        ];
-
-        /*$discount = [ // configuração de descontos
+            /*$discount = [ // configuração de descontos
             'type' => 'currency', // tipo de desconto a ser aplicado
             'value' => 599 // valor de desconto 
         ];
@@ -79,24 +108,24 @@ class GerencianetSdk
             'value' => 500, // porcentagem de desconto
             'until_date' => '2019-08-30' // data máxima para aplicação do desconto
         ];*/
-        $expiration_date = (new DateTime())->add(new DateInterval('P3D')); // quantidade de dias para vencimento 3 indica 3 dias
-        $expiration_date = ($expiration_date->format('Y-m-d'));
-        $bankingBillet = [
-            'expire_at' => $expiration_date, // data de vencimento do titulo
-            'message' => 'Pago em qualquer loterica\nPagar até o vencimento\nCaixa após vencimento não aceitar', // mensagem a ser exibida no boleto
-            'customer' => $customer,
-            /* 'discount' => $discount,
+            $expiration_date = (new DateTime())->add(new DateInterval('P3D')); // quantidade de dias para vencimento 3 indica 3 dias
+            $expiration_date = ($expiration_date->format('Y-m-d'));
+            $bankingBillet = [
+                'expire_at' => $expiration_date, // data de vencimento do titulo
+                'message' => 'Pago em qualquer loterica\nPagar até o vencimento\nCaixa após vencimento não aceitar', // mensagem a ser exibida no boleto
+                'customer' => $customer,
+                /* 'discount' => $discount,
             'conditional_discount' => $conditional_discount*/
-        ];
-        $payment = [
-            'banking_billet' => $bankingBillet // forma de pagamento (banking_billet = boleto)
-        ];
-        $body = [
-            'items' => $items,
-            'metadata' => $metadata,
-            'payment' => $payment
-        ];
-        try {
+            ];
+            $payment = [
+                'banking_billet' => $bankingBillet // forma de pagamento (banking_billet = boleto)
+            ];
+            $body = [
+                'items' => $items,
+                'metadata' => $metadata,
+                'payment' => $payment
+            ];
+
             $api = new Gerencianet($options);
             $pay_charge = $api->oneStep([], $body);
             return json_encode($pay_charge);
@@ -183,7 +212,7 @@ class GerencianetSdk
         $client_Secret,
         $nameClient,
         $emailClient,
-        $cpfClient,
+        $cpfCnpjClient,
         $phoneClient,
         $addressCep,
         $id,
@@ -196,50 +225,66 @@ class GerencianetSdk
         $clientId = $client_Id;
         $clientSecret = $client_Secret;
 
-        $new_unit_price = preg_replace('/[^0-9]/', '', number_format($unit_price, 2, '.', ''));
-
-        $phoneClient = preg_replace("/[^0-9]/", "", $phoneClient);
-        $cpfClient = preg_replace("/[^0-9]/", "", $cpfClient);
-        $addressCep = preg_replace("/[^0-9]/", "", $addressCep);
-
-        $expiration_date = strtotime($vencimento); // data de vencimento do boleto
-        $expiration_date = date('Y-m-d',$expiration_date);
-
-        $options = [
-            'client_id' => $clientId,
-            'client_secret' => $clientSecret,
-            'sandbox' => true
-        ];
-
-        $instructions = ["Pago em qualquer loterica", "Pagar até o vencimento", "Caixa após vencimento não aceitar"]; // Pode colocar até quatro instrunções
-
-        $item_1 = [
-            'name' => $title . " " . $id,
-            'amount' => (int) $quantity,
-            'value' => (int) $new_unit_price
-        ];
-
-        $items = [
-            $item_1
-        ];
-
-        $customer = [
-            'name' => $nameClient,
-            'cpf' => $cpfClient,
-            'phone_number' => $phoneClient,
-            'email' => $emailClient,
-        ];
-
-        $body = [
-            'items' => $items,
-            'repeats' => (int)$parcelas, // numero de parcelas que ira conter no boleto
-            'split_items' => false,
-            'expire_at' => $expiration_date,
-            'customer' => $customer,
-            'instructions' => $instructions
-        ];
-
         try {
+            if ($phoneClient != "" || $phoneClient != null) {
+                $phoneClient = preg_replace("/[^0-9]/", "",  $phoneClient);
+            } else {
+                throw new Exception('O celular do cliente precisa ser preenchido.');
+            }
+            if ($addressCep != "" || $addressCep != null) {
+                $addressCep = preg_replace("/[^0-9]/", "", $addressCep);
+            } else {
+                throw new Exception('O CEP do cliente precisa ser preenchido.');
+            }
+            if ($cpfCnpjClient != "" || $cpfCnpjClient != null) {
+                $cpfCnpjClient = preg_replace("/[^0-9]/", "", $cpfCnpjClient);
+            } else {
+                throw new Exception('O CPF/CNPJ do cliente precisa ser preenchido.');
+            }
+            if ($unit_price != "" && $unit_price != null && intval($unit_price) > 0) {
+                $new_unit_price = intval(preg_replace('/[^0-9]/', '', number_format($unit_price, 2, '.', '')));
+            } else {
+                throw new Exception('Não é possivel gerar pagamento com o valor R$  0,00.');
+            }
+
+
+            $expiration_date = strtotime($vencimento); // data de vencimento do boleto
+            $expiration_date = date('Y-m-d', $expiration_date);
+
+            $options = [
+                'client_id' => $clientId,
+                'client_secret' => $clientSecret,
+                'sandbox' => true
+            ];
+
+            $instructions = ["Pago em qualquer loterica", "Pagar até o vencimento", "Caixa após vencimento não aceitar"]; // Pode colocar até quatro instrunções
+
+            $item_1 = [
+                'name' => $title . " " . $id,
+                'amount' => (int) $quantity,
+                'value' => (int) $new_unit_price
+            ];
+
+            $items = [
+                $item_1
+            ];
+
+            $customer = [
+                'name' => $nameClient,
+                'cpf' => $cpfCnpjClient,
+                'phone_number' => $phoneClient,
+                'email' => $emailClient,
+            ];
+
+            $body = [
+                'items' => $items,
+                'repeats' => (int)$parcelas, // numero de parcelas que ira conter no boleto
+                'split_items' => false,
+                'expire_at' => $expiration_date,
+                'customer' => $customer,
+                'instructions' => $instructions
+            ];
+
             $api = new Gerencianet($options);
             $charge = $api->createCarnet([], $body);
             return json_encode($charge);
