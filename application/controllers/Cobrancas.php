491b90623628e0353d311bbac91f792ba6ebc020
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2020-12-30 08:29:02 -0300

    Create Cobrancas.php (Models)

---
+++ application/controllers/Cobrancas.php
@@ -0,0 +1,193 @@
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Cobrancas extends MY_Controller
+{
+
+    /**
+     * author: Ramon Silva
+     * email: silva018-mg@yahoo.com.br
+     *
+     */
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->helper('form');
+        $this->load->model('cobrancas_model');
+        $this->data['menuCobrancas'] = 'cobrancas';
+    }
+
+    public function index()
+    {
+        $this->cobrancas();
+    }
+
+    public function cobrancas()
+    {
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'vVenda')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para visualizar cobrancas.');
+            redirect(base_url());
+        }
+
+        $this->load->library('pagination');
+
+        $this->data['configuration']['base_url'] = site_url('cobrancas/cobrancas/');
+        $this->data['configuration']['total_rows'] = $this->cobrancas_model->count('cobrancas');
+
+        $this->pagination->initialize($this->data['configuration']);
+
+        $this->data['results'] = $this->cobrancas_model->get('cobrancas', '*', '', $this->data['configuration']['per_page'], $this->uri->segment(3));
+
+        $this->data['view'] = 'cobrancas/cobrancas';
+
+
+        return $this->layout();
+    }
+
+    public function excluir()
+    {
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'dVenda')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para excluir cobranças');
+            redirect(site_url('cobrancas/cobrancas/'));
+        }
+
+        $this->load->library('Gateways/GerencianetSdk', null, 'GerencianetSdk');
+        $this->load->model('pagamentos_model');
+        $change_id = $this->input->post('charge_id');
+        if ($change_id == null) {
+            $this->session->set_flashdata('error', 'Erro ao tentar excluir cobrança.');
+            redirect(site_url('cobrancas/cobrancas/'));
+        }
+        if ($this->cobrancas_model->delete('cobrancas', 'charge_id', $change_id) == true) {
+
+            $defaultPayment = $this->pagamentos_model->getPagamentos(0);
+            //Cancelamos o pagamento caso esteja em aberto
+            $pagamento = $this->GerencianetSdk->cancelarTransacao(
+                $change_id,
+                $defaultPayment->client_id,
+                $defaultPayment->client_secret
+            );
+
+            log_info('Removeu uma cobrança. ID' . $change_id);
+            $this->session->set_flashdata('success', 'Cobrança excluida com sucesso!');
+        } else {
+            $this->data['custom_error'] = '<div class="form_error"><p>Ocorreu um erro</p></div>';
+        }
+        redirect(site_url('cobrancas/cobrancas/'));
+    }
+
+    public function atualizar()
+    {
+        if (!$this->uri->segment(3) || !is_numeric($this->uri->segment(3))) {
+            $this->session->set_flashdata('error', 'Item não pode ser encontrado, parâmetro não foi passado corretamente.');
+            redirect('mapos');
+        }
+
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'vVenda')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para atualizar cobrança.');
+            redirect(base_url());
+        }
+
+        $this->load->library('Gateways/GerencianetSdk', null, 'GerencianetSdk');
+        $this->load->model('pagamentos_model');
+
+        $change_id = intval($this->uri->segment(3));
+        $defaultPayment = $this->pagamentos_model->getPagamentos(0);
+
+        //Pegamos o retorno para atualizar o banco
+        $pagamento = $this->GerencianetSdk->receberInfo(
+            $change_id,
+            $defaultPayment->client_id,
+            $defaultPayment->client_secret
+        );
+
+        $pagamento = json_decode($pagamento, true);
+        $obj = json_decode(json_encode($pagamento), FALSE);
+
+        $data = [
+
+            'status' => $obj->data->status,
+
+        ];
+
+        if ($this->pagamentos_model->edit('cobrancas', $data, 'charge_id',  $change_id) == true) {
+            $this->session->set_flashdata('success', 'Cobrança atualizada com sucesso!');
+            log_info('Alterou um status de cobrança. ID' .  $change_id);
+
+
+            //Cobrança foi paga ou foi confirmada de forma manual, então damo baixa 
+            if ($obj->data->status == "paid" || $obj->data->status == "settled") {
+            }
+        } else {
+            $this->data['custom_error'] = '<div class="form_error"><p>Ocorreu um erro</p></div>';
+        }
+
+
+        redirect(site_url('cobrancas/cobrancas/'));
+    }
+    public function cancelar()
+    {
+        if (!$this->uri->segment(3) || !is_numeric($this->uri->segment(3))) {
+            $this->session->set_flashdata('error', 'Item não pode ser encontrado, parâmetro não foi passado corretamente.');
+            redirect('mapos');
+        }
+
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'vVenda')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para cancelar cobrança.');
+            redirect(base_url());
+        }
+
+        $this->load->library('Gateways/GerencianetSdk', null, 'GerencianetSdk');
+        $this->load->model('pagamentos_model');
+
+        $change_id = intval($this->uri->segment(3));
+        $defaultPayment = $this->pagamentos_model->getPagamentos(0);
+
+        //Cancelamos o pagamento
+        $pagamento = $this->GerencianetSdk->cancelarTransacao(
+            $change_id,
+            $defaultPayment->client_id,
+            $defaultPayment->client_secret
+        );
+
+        $pagamento = json_decode($pagamento, true);
+        $obj = json_decode(json_encode($pagamento), FALSE);
+
+        if ($obj->code == '200') {
+            $this->session->set_flashdata('success', 'Cobrança cancelada com sucesso!');
+        } else {
+            $this->session->set_flashdata('error', $obj->errorDescription);
+
+            redirect(site_url('cobrancas/cobrancas/'));
+        }
+
+        //Pegamos o retorno para atualizar o banco
+        $pagamento = $this->GerencianetSdk->receberInfo(
+            $change_id,
+            $defaultPayment->client_id,
+            $defaultPayment->client_secret
+        );
+
+        $pagamento = json_decode($pagamento, true);
+        $obj = json_decode(json_encode($pagamento), FALSE);
+
+        $data = [
+
+            'status' => $obj->data->status,
+
+        ];
+
+        if ($this->pagamentos_model->edit('cobrancas', $data, 'charge_id',  $change_id) == true) {
+
+            log_info('Alterou um status de cobrança. ID' .  $change_id);
+        } else {
+            $this->data['custom_error'] = '<div class="form_error"><p>Ocorreu um erro</p></div>';
+        }
+
+
+        redirect(site_url('cobrancas/cobrancas/'));
+    }
+}
