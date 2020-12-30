f7624066c80f72ae41a4c689bcf5bcf96fd5d3c2
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2020-12-30 18:40:56 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -349,35 +349,44 @@ CREATE TABLE IF NOT EXISTS `vendas` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `cobrancas`
--- -----------------------------------------------------
+
+--
+-- Estrutura da tabela `cobrancas`
+--
+
 CREATE TABLE `cobrancas` (
-  `idCobranca` INT(4) NOT NULL AUTO_INCREMENT,
-  `charge_id` INT(4) NULL DEFAULT NULL,
-  `conditional_discount_date` DATE NULL DEFAULT NULL,
-  `created_at` DATETIME NULL DEFAULT NULL,
-  `custom_id` INT(4) NULL DEFAULT NULL,
-  `expire_at` DATE NOT NULL,
-  `message` VARCHAR(255) NOT NULL,
-  `payment_method` VARCHAR(11) NULL DEFAULT NULL,
-  `payment_url` VARCHAR(255) NULL DEFAULT NULL,
-  `request_delivery_address` VARCHAR(64) NULL DEFAULT NULL,
-  `status` VARCHAR(36) NOT NULL,
-  `total` VARCHAR(15) NULL DEFAULT NULL,
-  `barcode` VARCHAR(255) NOT NULL,
-  `link` VARCHAR(255) NOT NULL,
-  `payment` VARCHAR(64) NOT NULL,
-  `pdf` VARCHAR(255) NULL DEFAULT NULL,
-  `vendas_id` INT(11) NOT NULL,
-  PRIMARY KEY (`idCobranca`),
-  INDEX `fk_cobrancas_vendas1_idx` (`vendas_id` ASC) ,
-  CONSTRAINT `fk_cobrancas_vendas1`
-    FOREIGN KEY (`vendas_id`)
-    REFERENCES `mapos`.`vendas` (`idVendas`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  `idCobranca` int(4) NOT NULL,
+  `charge_id` int(4) DEFAULT NULL,
+  `conditional_discount_date` date DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `custom_id` int(4) DEFAULT NULL,
+  `expire_at` date NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `payment_method` varchar(11) DEFAULT NULL,
+  `payment_url` varchar(255) DEFAULT NULL,
+  `request_delivery_address` varchar(64) DEFAULT NULL,
+  `status` varchar(36) NOT NULL,
+  `total` varchar(15) DEFAULT NULL,
+  `barcode` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `payment` varchar(64) NOT NULL,
+  `pdf` varchar(255) DEFAULT NULL,
+  `vendas_id` int(11) DEFAULT NULL,
+  `os_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tabela `cobrancas`
+--
+ALTER TABLE `cobrancas`
+  ADD PRIMARY KEY (`idCobranca`);
+
+--
+-- AUTO_INCREMENT de tabela `cobrancas`
+--
+
+ALTER TABLE `cobrancas`
+  MODIFY `idCobranca` int(4) NOT NULL AUTO_INCREMENT;
 
 -- -----------------------------------------------------
 -- Table `itens_de_vendas`
