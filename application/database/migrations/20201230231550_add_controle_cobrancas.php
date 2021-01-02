f2f6d68b56913968184fa60677c42e408e4c5a1a
Wilmerson <will.phelipe@gmail.com>
2021-01-02 12:56:27 -0300

    Add FK em venda_id e os_id. (#1105)
    * Add FK em venda_id e os_id (gerenciamento de cobranças).
    
    * Adicionado permissão ao administrado do campo cobrança como ativo por padrão.

--- application/database/migrations/20201230231550_add_controle_cobrancas.php
+++ application/database/migrations/20201230231550_add_controle_cobrancas.php
@@ -96,6 +96,20 @@ class Migration_add_controle_cobrancas extends CI_Migration
         ]);
         $this->dbforge->add_key("idCobranca", true);
         $this->dbforge->create_table("cobrancas", true);
+        $this->db->query('ALTER TABLE  `cobrancas` ADD INDEX `fk_cobrancas_os1` (`os_id` ASC)');
+        $this->db->query('ALTER TABLE  `cobrancas` ADD CONSTRAINT `fk_cobrancas_os1`
+			FOREIGN KEY (`os_id`)
+			REFERENCES `os` (`idOs`)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
+        ');
+        $this->db->query('ALTER TABLE  `cobrancas` ADD INDEX `fk_cobrancas_vendas1` (`vendas_id` ASC)');
+        $this->db->query('ALTER TABLE  `cobrancas` ADD CONSTRAINT `fk_cobrancas_vendas1`
+			FOREIGN KEY (`vendas_id`)
+			REFERENCES `vendas` (`idVendas`)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
+		');
         $this->db->query('ALTER TABLE `cobrancas` ENGINE = InnoDB');
     }
 
