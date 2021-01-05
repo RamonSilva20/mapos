22df050ee5c82cf8046a9c7d3f5ebfe4fb12e74f
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-05 13:59:42 -0300

    run php fixer

--- application/views/conecte/login.php
+++ application/views/conecte/login.php
@@ -54,8 +54,10 @@
             <div class="control-group">
                 <div class="controls">
                     <div class="main_input_box">
-                        <span class="add-on bg_lg"><i class="fas fa-user"></i></span><input id="email" name="email" type="text" placeholder="Email" value="<?php if (isset($_GET['e'])) { echo $_GET['e']; ?>
-                            <?php } ?>
+                        <span class="add-on bg_lg"><i class="fas fa-user"></i></span><input id="email" name="email" type="text" placeholder="Email" value="<?php if (isset($_GET['e'])) {
+    echo $_GET['e']; ?>
+                            <?php
+} ?>
                          "/>
                     </div>
                 </div>
@@ -63,8 +65,10 @@
             <div class="control-group">
                 <div class="controls">
                     <div class="main_input_box">
-                        <span class="add-on bg_ly"><i class="fas fa-id-card"></i></span><input class="cpfcnpj" maxlength="18" size="18" name="documento" type="text" placeholder="CPF/CNPJ" OnKeyPress="formatar('000.000.000/0000-00', this)" value="<?php if (isset($_GET['c'])) { echo $_GET['c']; ?>
-                            <?php } ?>
+                        <span class="add-on bg_ly"><i class="fas fa-id-card"></i></span><input class="cpfcnpj" maxlength="18" size="18" name="documento" type="text" placeholder="CPF/CNPJ" OnKeyPress="formatar('000.000.000/0000-00', this)" value="<?php if (isset($_GET['c'])) {
+        echo $_GET['c']; ?>
+                            <?php
+    } ?>
                          "/>
                     </div>
                 </div>
