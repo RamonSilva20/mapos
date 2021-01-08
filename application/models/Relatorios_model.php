f154947addfa1ca904f57e31ae6061ec2f16dc58
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-07 21:18:58 -0300

    fix: corrige um erro no relatorio de vendas

--- application/models/Relatorios_model.php
+++ application/models/Relatorios_model.php
@@ -336,10 +336,11 @@ class Relatorios_model extends CI_Model
 
     public function vendasRapid($array = false)
     {
-        $this->db->select('vendas.*,clientes.nomeCliente, usuarios.nome');
+        $this->db->select('vendas.*,clientes.nomeCliente, usuarios.nome,itens_de_vendas.subTotal as valorTotal, itens_de_vendas.vendas_id');
         $this->db->from('vendas');
         $this->db->join('clientes', 'clientes.idClientes = vendas.clientes_id');
         $this->db->join('usuarios', 'usuarios.idUsuarios = vendas.usuarios_id');
+        $this->db->join('itens_de_vendas', 'itens_de_vendas.vendas_id = vendas.idVendas');
         $this->db->order_by('vendas.dataVenda', 'DESC');
 
         $result = $this->db->get();
