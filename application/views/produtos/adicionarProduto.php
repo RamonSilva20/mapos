ede36530a8c7cbae370c664f5084505a752b5586
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-21 13:15:22 -0300

    fix: adiciona mais opções de unidades de medidas de produtos seguindo o modelo da SEFAZ-PA

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -77,15 +77,7 @@
                     <div class="control-group">
                         <label for="unidade" class="control-label">Unidade<span class="required">*</span></label>
                         <div class="controls">
-                            <!--<input id="unidade" type="text" name="unidade" value="<?php echo set_value('unidade'); ?>"  />-->
-                            <select id="unidade" name="unidade">
-                                <option value="UN">Unidade</option>
-                                <option value="KG">Kilograma</option>
-                                <option value="LT">Litro</option>
-                                <option value="CX">Caixa</option>
-                                <option value="M2">M²</option>
-                                <option value="OT">Outro</option>
-                            </select>
+                            <select id="unidade" name="unidade"></select>
                         </div>
                     </div>
                     <div class="control-group">
@@ -118,6 +110,11 @@
 <script type="text/javascript">
     $(document).ready(function() {
         $(".money").maskMoney();
+        $.getJSON('<?php echo base_url() ?>assets/json/tabela_medidas.json', function(data) {
+            for (i in data.medidas) {
+                   $('#unidade').append(new Option(data.medidas[i].descricao, data.medidas[i].sigla));
+            }
+        });
         $('#formProduto').validate({
             rules: {
                 descricao: {
