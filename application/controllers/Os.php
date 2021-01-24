6ae8200a25d45d8104e812630a910fd24d6761e3
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-24 12:33:59 -0300

    fix: corrige o valor da OS no wpp

--- application/controllers/Os.php
+++ application/controllers/Os.php
@@ -275,7 +275,16 @@ class Os extends MY_Controller
         $this->data['servicos'] = $this->os_model->getServicos($this->uri->segment(3));
         $this->data['anexos'] = $this->os_model->getAnexos($this->uri->segment(3));
         $this->data['anotacoes'] = $this->os_model->getAnotacoes($this->uri->segment(3));
-
+        $this->data['totalServico'] = 0;
+        $this->data['totalProdutos'] = 0; 
+        foreach ($this->data['produtos'] as $p) {
+            $this->data['totalProdutos'] = $this->data['totalProdutos'] + $p->subTotal;
+        }
+        foreach ($this->data['servicos'] as $s) {
+            $preco = $s->preco ?: $s->precoVenda;
+            $subtotal = $preco * ($s->quantidade ?: 1);
+            $this->data['totalServico'] = $this->data['totalServico'] + $subtotal;
+        }
         $this->load->model('mapos_model');
         $this->data['emitente'] = $this->mapos_model->getEmitente();
 
