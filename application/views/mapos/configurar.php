2d09a1518c06abe3965d731324b7b2b3c5676b8a
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-24 14:17:19 -0300

    Update configurar.php

--- application/views/mapos/configurar.php
+++ application/views/mapos/configurar.php
@@ -148,7 +148,7 @@
         <div class="modal-body">
             <h5 style="text-align: left">Deseja realmente fazer a atualização do banco de dados?</h5>
             <h7 style="text-align: left">Recomendamos que faça um backup antes de prosseguir!  
-            <a target="_blank" title="Imprimir OS" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/mapos/backup">Fazer Backup</a></h7>
+            <a target="_blank" title="Fazer Bakup" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/mapos/backup">Fazer Backup</a></h7>
         </div>
         <div class="modal-footer">
             <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
