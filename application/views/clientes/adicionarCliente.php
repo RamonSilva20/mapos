8bc20fa9e02050273e80a9f21d60b0f3c7fb2c51
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-25 10:19:06 -0300

    fix: melhoria na tela de cadastrar clientes e link no nome do cliente indo para visualizar cliente

--- application/views/clientes/adicionarCliente.php
+++ application/views/clientes/adicionarCliente.php
@@ -1,211 +1,218 @@
 <script src="<?php echo base_url()?>assets/js/jquery.mask.min.js"></script>
 <script src="<?php echo base_url()?>assets/js/sweetalert2.all.min.js"></script>
 <script src="<?php echo base_url()?>assets/js/funcoes.js"></script>
-
 <div class="row-fluid" style="margin-top:0">
-    <div class="span12">
-        <div class="widget-box">
-            <div class="widget-title">
-                <span class="icon">
-                    <i class="fas fa-user"></i>
-                </span>
-                <h5>Cadastro de Cliente</h5>
+   <div class="span12">
+      <div class="widget-box">
+         <div class="widget-title">
+            <span class="icon">
+            <i class="fas fa-user"></i>
+            </span>
+            <h5>Cadastro de Cliente</h5>
+            <div class="buttons">
+               <a title="Voltar" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/clientes"><i class="fas fa-arrow-left"></i> Voltar</a>
             </div>
+         </div>
+         <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">Informações Pessoais</a></li>
+            <li><a data-toggle="tab" href="#menu2">Endereço</a></li>
+         </ul>
+         <form action="<?php echo current_url(); ?>" id="formCliente" method="post" class="form-horizontal">
             <div class="widget-content nopadding tab-content">
-                <?php if ($custom_error != '') {
-    echo '<div class="alert alert-danger">' . $custom_error . '</div>';
-} ?>
-                <form action="<?php echo current_url(); ?>" id="formCliente" method="post" class="form-horizontal">
+               <?php if ($custom_error != '') {
+                  echo '<div class="alert alert-danger">' . $custom_error . '</div>';
+                  } ?>
+               <div id="home" class="tab-pane fade in active">
                   <div class="control-group">
-                      <label for="documento" class="control-label">CPF/CNPJ<span class="required">*</span></label>
-                      <div class="controls">
-                          <input id="documento" class="cpfcnpj" type="text" name="documento" value="<?php echo set_value('documento'); ?>"  />
-                          <button id="buscar_info_cnpj" class="btn btn-xs" type="button">Buscar Informações (CNPJ)</button>
-                      </div>
+                     <label for="documento" class="control-label">CPF/CNPJ<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="documento" class="cpfcnpj" type="text" name="documento" value="<?php echo set_value('documento'); ?>"  />
+                        <button id="buscar_info_cnpj" class="btn btn-xs" type="button">Buscar Informações (CNPJ)</button>
+                     </div>
                   </div>
                   <div class="control-group">
-                        <label for="nomeCliente" class="control-label">Nome<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="nomeCliente" type="text" name="nomeCliente" value="<?php echo set_value('nomeCliente'); ?>" />
+                     <label for="nomeCliente" class="control-label">Nome<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="nomeCliente" type="text" name="nomeCliente" value="<?php echo set_value('nomeCliente'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group">
+                     <label for="contato" class="control-label">Contato:</label>
+                     <div class="controls">
+                        <input class="nomeCliente" type="text" name="contato" value="<?php echo set_value('contato'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group">
+                     <label for="telefone" class="control-label">Telefone<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="telefone" type="text" name="telefone" value="<?php echo set_value('telefone'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group">
+                     <label for="celular" class="control-label">Celular</label>
+                     <div class="controls">
+                        <input id="celular" type="text" name="celular" value="<?php echo set_value('celular'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group">
+                     <label for="email" class="control-label">Email<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="email" type="text" name="email" value="<?php echo set_value('email'); ?>" />
+                     </div>
+                  </div>
+                  <div class="form-actions">
+                     <div class="span12">
+                        <div class="span6 offset3">
+                           <button type="submit" class="btn btn-success">Salvar</button>
                         </div>
-                    </div>
-                    <div class="control-group">
-                        <label for="contato" class="control-label">Contato:</label>
-                        <div class="controls">
-                            <input class="nomeCliente" type="text" name="contato" value="<?php echo set_value('contato'); ?>" />
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label for="telefone" class="control-label">Telefone<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="telefone" type="text" name="telefone" value="<?php echo set_value('telefone'); ?>" />
-                        </div>
-                    </div>
-
-                    <div class="control-group">
-                        <label for="celular" class="control-label">Celular</label>
-                        <div class="controls">
-                            <input id="celular" type="text" name="celular" value="<?php echo set_value('celular'); ?>" />
-                        </div>
-                    </div>
-
-                    <div class="control-group">
-                        <label for="email" class="control-label">Email<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="email" type="text" name="email" value="<?php echo set_value('email'); ?>" />
-                        </div>
-                    </div>
-                    <div class="control-group" class="control-label">
-                        <label for="cep" class="control-label">CEP<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="cep" type="text" name="cep" value="<?php echo set_value('cep'); ?>" />
-                        </div>
-                    </div>
-                    <div class="control-group" class="control-label">
-                        <label for="rua" class="control-label">Rua<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="rua" type="text" name="rua" value="<?php echo set_value('rua'); ?>" />
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label for="numero" class="control-label">Número<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="numero" type="text" name="numero" value="<?php echo set_value('numero'); ?>" />
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label for="complemento" class="control-label">Complemento</label>
-                        <div class="controls">
-                            <input id="complemento" type="text" name="complemento" value="<?php echo set_value('complemento'); ?>" />
-                        </div>
-                    </div>
-
-                    <div class="control-group" class="control-label">
-                        <label for="bairro" class="control-label">Bairro<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="bairro" type="text" name="bairro" value="<?php echo set_value('bairro'); ?>" />
-                        </div>
-                    </div>
-
-                    <div class="control-group" class="control-label">
-                        <label for="cidade" class="control-label">Cidade<span class="required">*</span></label>
-                        <div class="controls">
-                            <input id="cidade" type="text" name="cidade" value="<?php echo set_value('cidade'); ?>" />
-                        </div>
-                    </div>
-
-                    <div class="control-group" class="control-label">
-                        <label for="estado" class="control-label">Estado<span class="required">*</span></label>
-                        <div class="controls">
+                     </div>
+                  </div>
+               </div>
+               <!-- Menu Endereços -->
+               <div id="menu2" class="tab-pane fade">
+                  <div class="control-group" class="control-label">
+                     <label for="cep" class="control-label">CEP<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="cep" type="text" name="cep" value="<?php echo set_value('cep'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group" class="control-label">
+                     <label for="rua" class="control-label">Rua<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="rua" type="text" name="rua" value="<?php echo set_value('rua'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group">
+                     <label for="numero" class="control-label">Número<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="numero" type="text" name="numero" value="<?php echo set_value('numero'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group">
+                     <label for="complemento" class="control-label">Complemento</label>
+                     <div class="controls">
+                        <input id="complemento" type="text" name="complemento" value="<?php echo set_value('complemento'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group" class="control-label">
+                     <label for="bairro" class="control-label">Bairro<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="bairro" type="text" name="bairro" value="<?php echo set_value('bairro'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group" class="control-label">
+                     <label for="cidade" class="control-label">Cidade<span class="required">*</span></label>
+                     <div class="controls">
+                        <input id="cidade" type="text" name="cidade" value="<?php echo set_value('cidade'); ?>" />
+                     </div>
+                  </div>
+                  <div class="control-group" class="control-label">
+                     <label for="estado" class="control-label">Estado<span class="required">*</span></label>
+                     <div class="controls">
                         <select id="estado" name="estado">
-                              <option value="">Selecione...</option>
+                           <option value="">Selecione...</option>
                         </select>
+                     </div>
+                  </div>
+                  <div class="form-actions">
+                     <div class="span12">
+                        <div class="span6 offset3">
+                           <button type="submit" class="btn btn-success">Salvar</button>
                         </div>
-                    </div>
-
-
-
-                    <div class="form-actions">
-                        <div class="span12">
-                            <div class="span6 offset3">
-                                <button type="submit" class="btn btn-success"><i class="fas fa-plus"></i> Adicionar</button>
-                                <a href="<?php echo base_url() ?>index.php/clientes" id="" class="btn"><i class="fas fa-backward"></i> Voltar</a>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+                     </div>
+                  </div>
+         </form>
+         </div>
+         </div>
+      </div>
+   </div>
 </div>
-
-
 <script src="<?php echo base_url() ?>assets/js/jquery.validate.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $.getJSON('<?php echo base_url() ?>assets/json/estados.json', function(data) {
-            for (i in data.estados) {
-                   $('#estado').append(new Option(data.estados[i].nome, data.estados[i].sigla));
-                   var curState = '<?php echo set_value('estado'); ?>';
-                   if (curState) { $("#estado option[value=" + curState + "]").prop("selected",true); }
-            }
-        });
-        $('#formCliente').validate({
-            rules: {
-                nomeCliente: {
-                    required: true
-                },
-                documento: {
-                    required: true
-                },
-                telefone: {
-                    required: true
-                },
-                email: {
-                    required: true
-                },
-                rua: {
-                    required: true
-                },
-                numero: {
-                    required: true
-                },
-                bairro: {
-                    required: true
-                },
-                cidade: {
-                    required: true
-                },
-                estado: {
-                    required: true
-                },
-                cep: {
-                    required: true
-                }
-            },
-            messages: {
-                nomeCliente: {
-                    required: 'Campo Requerido.'
-                },
-                documento: {
-                    required: 'Campo Requerido.'
-                },
-                telefone: {
-                    required: 'Campo Requerido.'
-                },
-                email: {
-                    required: 'Campo Requerido.'
-                },
-                rua: {
-                    required: 'Campo Requerido.'
-                },
-                numero: {
-                    required: 'Campo Requerido.'
-                },
-                bairro: {
-                    required: 'Campo Requerido.'
-                },
-                cidade: {
-                    required: 'Campo Requerido.'
-                },
-                estado: {
-                    required: 'Campo Requerido.'
-                },
-                cep: {
-                    required: 'Campo Requerido.'
-                }
-
-            },
-
-            errorClass: "help-inline",
-            errorElement: "span",
-            highlight: function(element, errorClass, validClass) {
-                $(element).parents('.control-group').addClass('error');
-            },
-            unhighlight: function(element, errorClass, validClass) {
-                $(element).parents('.control-group').removeClass('error');
-                $(element).parents('.control-group').addClass('success');
-            }
-        });
-    });
+   $(document).ready(function() {
+       $.getJSON('<?php echo base_url() ?>assets/json/estados.json', function(data) {
+           for (i in data.estados) {
+                  $('#estado').append(new Option(data.estados[i].nome, data.estados[i].sigla));
+                  var curState = '<?php echo set_value('estado'); ?>';
+                  if (curState) { $("#estado option[value=" + curState + "]").prop("selected",true); }
+           }
+       });
+       $('#formCliente').validate({
+           rules: {
+               nomeCliente: {
+                   required: true
+               },
+               documento: {
+                   required: true
+               },
+               telefone: {
+                   required: true
+               },
+               email: {
+                   required: true
+               },
+               rua: {
+                   required: true
+               },
+               numero: {
+                   required: true
+               },
+               bairro: {
+                   required: true
+               },
+               cidade: {
+                   required: true
+               },
+               estado: {
+                   required: true
+               },
+               cep: {
+                   required: true
+               }
+           },
+           messages: {
+               nomeCliente: {
+                   required: 'Campo Requerido.'
+               },
+               documento: {
+                   required: 'Campo Requerido.'
+               },
+               telefone: {
+                   required: 'Campo Requerido.'
+               },
+               email: {
+                   required: 'Campo Requerido.'
+               },
+               rua: {
+                   required: 'Campo Requerido.'
+               },
+               numero: {
+                   required: 'Campo Requerido.'
+               },
+               bairro: {
+                   required: 'Campo Requerido.'
+               },
+               cidade: {
+                   required: 'Campo Requerido.'
+               },
+               estado: {
+                   required: 'Campo Requerido.'
+               },
+               cep: {
+                   required: 'Campo Requerido.'
+               }
+   
+           },
+   
+           errorClass: "help-inline",
+           errorElement: "span",
+           highlight: function(element, errorClass, validClass) {
+               $(element).parents('.control-group').addClass('error');
+           },
+           unhighlight: function(element, errorClass, validClass) {
+               $(element).parents('.control-group').removeClass('error');
+               $(element).parents('.control-group').addClass('success');
+           }
+       });
+   });
 </script>
