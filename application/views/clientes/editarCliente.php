ca1de41e687b30dad396b6042169d09f3b43e99a
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-26 20:35:43 -0300

    fix: remove a obrigatoriedade nos campos em editar cliente

--- application/views/clientes/editarCliente.php
+++ application/views/clientes/editarCliente.php
@@ -6,20 +6,23 @@
     .badgebox {
         opacity: 0;
     }
-    .badgebox+.badge {
+
+    .badgebox + .badge {
         /* Move the check mark away when unchecked */
         text-indent: -999999px;
         /* Makes the badge's width stay the same checked and unchecked */
         width: 27px;
     }
-    .badgebox:focus+.badge {
+
+    .badgebox:focus + .badge {
         /* Set something to make the badge looks focused */
         /* This really depends on the application, in my case it was: */
         /* Adding a light border */
         box-shadow: inset 0px 0px 5px;
         /* Taking the difference out of the padding */
     }
-    .badgebox:checked+.badge {
+
+    .badgebox:checked + .badge {
         /* Move the check mark back when checked */
         text-indent: 0;
     }
@@ -33,7 +36,8 @@
             </span>
                 <h5>Editar Cliente</h5>
                 <div class="buttons">
-                    <a title="Voltar" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/clientes"><i class="fas fa-arrow-left"></i> Voltar</a>
+                    <a title="Voltar" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/clientes"><i
+                                class="fas fa-arrow-left"></i> Voltar</a>
                 </div>
             </div>
             <ul class="nav nav-tabs">
@@ -49,46 +53,54 @@
                         <div class="control-group">
                             <label for="documento" class="control-label">CPF/CNPJ</label>
                             <div class="controls">
-                                <input id="documento" class="cpfcnpj" type="text" name="documento" value="<?php echo $result->documento; ?>" />
-                                <button id="buscar_info_cnpj" class="btn btn-xs" type="button">Buscar Informações (CNPJ)</button>
+                                <input id="documento" class="cpfcnpj" type="text" name="documento"
+                                       value="<?php echo $result->documento; ?>"/>
+                                <button id="buscar_info_cnpj" class="btn btn-xs" type="button">Buscar Informações
+                                    (CNPJ)
+                                </button>
                             </div>
                         </div>
                         <div class="control-group">
                             <?php echo form_hidden('idClientes', $result->idClientes) ?>
-                            <label for="nomeCliente" class="control-label">Nome/Razão Social<span class="required">*</span></label>
+                            <label for="nomeCliente" class="control-label">Nome/Razão Social<span
+                                        class="required">*</span></label>
                             <div class="controls">
-                                <input id="nomeCliente" type="text" name="nomeCliente" value="<?php echo $result->nomeCliente; ?>" />
+                                <input id="nomeCliente" type="text" name="nomeCliente"
+                                       value="<?php echo $result->nomeCliente; ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="contato" class="control-label">Contato:</label>
                             <div class="controls">
-                                <input class="nomeCliente" type="text" name="contato" value="<?php echo $result->contato; ?>" />
+                                <input class="nomeCliente" type="text" name="contato"
+                                       value="<?php echo $result->contato; ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="telefone" class="control-label">Telefone</label>
                             <div class="controls">
-                                <input id="telefone" type="text" name="telefone" value="<?php echo $result->telefone; ?>" />
+                                <input id="telefone" type="text" name="telefone"
+                                       value="<?php echo $result->telefone; ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="celular" class="control-label">Celular</label>
                             <div class="controls">
-                                <input id="celular" type="text" name="celular" value="<?php echo $result->celular; ?>" />
+                                <input id="celular" type="text" name="celular" value="<?php echo $result->celular; ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="email" class="control-label">Email</label>
                             <div class="controls">
-                                <input id="email" type="text" name="email" value="<?php echo $result->email; ?>" />
+                                <input id="email" type="text" name="email" value="<?php echo $result->email; ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label class="control-label">Tipo de Cliente</label>
                             <div class="controls">
                                 <label for="fornecedor" class="btn btn-default" style="margin-top: 5px;">Fornecedor
-                                    <input type="checkbox" id="fornecedor" name="fornecedor" class="badgebox" value="1" <?= ($result->fornecedor == 1) ? 'checked' : '' ?>>
+                                    <input type="checkbox" id="fornecedor" name="fornecedor" class="badgebox"
+                                           value="1" <?= ($result->fornecedor == 1) ? 'checked' : '' ?>>
                                     <span class="badge">&check;</span>
                                 </label>
                             </div>
@@ -96,7 +108,9 @@
                         <div class="form-actions">
                             <div class="span12">
                                 <div class="span6 offset3">
-                                    <button type="submit" class="btn btn-primary"><i class="fas fa-sync-alt"></i> Atualizar</button>
+                                    <button type="submit" class="btn btn-primary"><i class="fas fa-sync-alt"></i>
+                                        Atualizar
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -106,37 +120,38 @@
                         <div class="control-group" class="control-label">
                             <label for="cep" class="control-label">CEP</label>
                             <div class="controls">
-                                <input id="cep" type="text" name="cep" value="<?php echo $result->cep; ?>" />
+                                <input id="cep" type="text" name="cep" value="<?php echo $result->cep; ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
                             <label for="rua" class="control-label">Rua</label>
                             <div class="controls">
-                                <input id="rua" type="text" name="rua" value="<?php echo $result->rua; ?>" />
+                                <input id="rua" type="text" name="rua" value="<?php echo $result->rua; ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="numero" class="control-label">Número</label>
                             <div class="controls">
-                                <input id="numero" type="text" name="numero" value="<?php echo $result->numero; ?>" />
+                                <input id="numero" type="text" name="numero" value="<?php echo $result->numero; ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="complemento" class="control-label">Complemento</label>
                             <div class="controls">
-                                <input id="complemento" type="text" name="complemento" value="<?php echo $result->complemento; ?>" />
+                                <input id="complemento" type="text" name="complemento"
+                                       value="<?php echo $result->complemento; ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
                             <label for="bairro" class="control-label">Bairro</label>
                             <div class="controls">
-                                <input id="bairro" type="text" name="bairro" value="<?php echo $result->bairro; ?>" />
+                                <input id="bairro" type="text" name="bairro" value="<?php echo $result->bairro; ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
                             <label for="cidade" class="control-label">Cidade</label>
                             <div class="controls">
-                                <input id="cidade" type="text" name="cidade" value="<?php echo $result->cidade; ?>" />
+                                <input id="cidade" type="text" name="cidade" value="<?php echo $result->cidade; ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
@@ -150,7 +165,9 @@
                         <div class="form-actions">
                             <div class="span12">
                                 <div class="span6 offset3">
-                                    <button type="submit" class="btn btn-primary"><i class="fas fa-sync-alt"></i> Atualizar</button>
+                                    <button type="submit" class="btn btn-primary"><i class="fas fa-sync-alt"></i>
+                                        Atualizar
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -162,12 +179,14 @@
 </div>
 <script src="<?php echo base_url() ?>assets/js/jquery.validate.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $.getJSON('<?php echo base_url() ?>assets/json/estados.json', function(data) {
+    $(document).ready(function () {
+        $.getJSON('<?php echo base_url() ?>assets/json/estados.json', function (data) {
             for (i in data.estados) {
                 $('#estado').append(new Option(data.estados[i].nome, data.estados[i].sigla));
                 var curState = '<?php echo $result->estado; ?>';
-                if (curState) { $("#estado option[value=" + curState + "]").prop("selected",true); }
+                if (curState) {
+                    $("#estado option[value=" + curState + "]").prop("selected", true);
+                }
             }
         });
         $('#formCliente').validate({
@@ -175,77 +194,22 @@
                 nomeCliente: {
                     required: true
                 },
-                documento: {
-                    required: true
-                },
-                telefone: {
-                    required: true
-                },
-                email: {
-                    required: true
-                },
-                rua: {
-                    required: true
-                },
-                numero: {
-                    required: true
-                },
-                bairro: {
-                    required: true
-                },
-                cidade: {
-                    required: true
-                },
-                estado: {
-                    required: true
-                },
-                cep: {
-                    required: true
-                }
             },
             messages: {
                 nomeCliente: {
                     required: 'Campo Requerido.'
                 },
-                documento: {
-                    required: 'Campo Requerido.'
-                },
-                telefone: {
-                    required: 'Campo Requerido.'
-                },
-                email: {
-                    required: 'Campo Requerido.'
-                },
-                rua: {
-                    required: 'Campo Requerido.'
-                },
-                numero: {
-                    required: 'Campo Requerido.'
-                },
-                bairro: {
-                    required: 'Campo Requerido.'
-                },
-                cidade: {
-                    required: 'Campo Requerido.'
-                },
-                estado: {
-                    required: 'Campo Requerido.'
-                },
-                cep: {
-                    required: 'Campo Requerido.'
-                }
-
             },
 
             errorClass: "help-inline",
             errorElement: "span",
-            highlight: function(element, errorClass, validClass) {
+            highlight: function (element, errorClass, validClass) {
                 $(element).parents('.control-group').addClass('error');
             },
-            unhighlight: function(element, errorClass, validClass) {
+            unhighlight: function (element, errorClass, validClass) {
                 $(element).parents('.control-group').removeClass('error');
                 $(element).parents('.control-group').addClass('success');
-            }
+            } 
         });
     });
 </script>
