8e84d5498131a9297d0ee16aa64486e52869a51c
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-26 19:02:27 -0300

    fix: melorias na tela de cadastro visualização e edição de clientes

--- application/views/clientes/visualizar.php
+++ application/views/clientes/visualizar.php
@@ -5,15 +5,14 @@
             <li><a data-toggle="tab" href="#tab2">Ordens de Serviço</a></li>
             <div class="buttons">
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'eCliente')) {
-    echo '<a title="Icon Title" class="btn btn-mini btn-info" href="' . base_url() . 'index.php/clientes/editar/' . $result->idClientes . '"><i class="fas fa-edit"></i> Editar</a>';
-} ?>
-
+                    echo '<a title="Icon Title" class="btn btn-mini btn-info" href="' . base_url() . 'index.php/clientes/editar/' . $result->idClientes . '"><i class="fas fa-edit"></i> Editar</a>';
+                } ?>
+                <a title="Voltar" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/clientes"><i class="fas fa-arrow-left"></i> Voltar</a>
             </div>
         </ul>
     </div>
     <div class="widget-content tab-content">
         <div id="tab1" class="tab-pane active" style="min-height: 300px">
-
             <div class="accordion" id="collapse-group">
                 <div class="accordion-group widget-box">
                     <div class="accordion-heading">
@@ -28,24 +27,30 @@
                         <div class="widget-content">
                             <table class="table table-bordered">
                                 <tbody>
-                                    <tr>
-                                        <td style="text-align: right; width: 30%"><strong>Nome</strong></td>
-                                        <td>
-                                            <?php echo $result->nomeCliente ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Documento</strong></td>
-                                        <td>
-                                            <?php echo $result->documento ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Data de Cadastro</strong></td>
-                                        <td>
-                                            <?php echo date('d/m/Y', strtotime($result->dataCadastro)) ?>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td style="text-align: right; width: 30%"><strong>Nome</strong></td>
+                                    <td>
+                                        <?php echo $result->nomeCliente ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Documento</strong></td>
+                                    <td>
+                                        <?php echo $result->documento ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Data de Cadastro</strong></td>
+                                    <td>
+                                        <?php echo date('d/m/Y', strtotime($result->dataCadastro)) ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Tipo do Cliente</strong></td>
+                                    <td>
+                                        <?php echo $result->fornecedor == true ? 'Fornecedor' : 'Cliente'; ?>
+                                    </td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
@@ -64,30 +69,30 @@
                         <div class="widget-content">
                             <table class="table table-bordered">
                                 <tbody>
-                                    <tr>
-                                        <td style="text-align: right; width: 30%"><strong>Contato:</strong></td>
-                                        <td>
-                                            <?php echo $result->contato ?>
-                                        </td>
-                                    </tr>
-                                        <tr>
-                                            <td style="text-align: right; width: 30%"><strong>Telefone</strong></td>
-                                            <td>
-                                                <?php echo $result->telefone ?>
-                                            </td>
-                                        </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Celular</strong></td>
-                                        <td>
-                                            <?php echo $result->celular ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Email</strong></td>
-                                        <td>
-                                            <?php echo $result->email ?>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td style="text-align: right; width: 30%"><strong>Contato:</strong></td>
+                                    <td>
+                                        <?php echo $result->contato ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right; width: 30%"><strong>Telefone</strong></td>
+                                    <td>
+                                        <?php echo $result->telefone ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Celular</strong></td>
+                                    <td>
+                                        <?php echo $result->celular ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Email</strong></td>
+                                    <td>
+                                        <?php echo $result->email ?>
+                                    </td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
@@ -106,129 +111,112 @@
                         <div class="widget-content">
                             <table class="table table-bordered">
                                 <tbody>
-                                    <tr>
-                                        <td style="text-align: right; width: 30%"><strong>Rua</strong></td>
-                                        <td>
-                                            <?php echo $result->rua ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Número</strong></td>
-                                        <td>
-                                            <?php echo $result->numero ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Complemento</strong></td>
-                                        <td>
-                                            <?php echo $result->complemento ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Bairro</strong></td>
-                                        <td>
-                                            <?php echo $result->bairro ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>Cidade</strong></td>
-                                        <td>
-                                            <?php echo $result->cidade ?> -
-                                            <?php echo $result->estado ?>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: right"><strong>CEP</strong></td>
-                                        <td>
-                                            <?php echo $result->cep ?>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td style="text-align: right; width: 30%"><strong>Rua</strong></td>
+                                    <td>
+                                        <?php echo $result->rua ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Número</strong></td>
+                                    <td>
+                                        <?php echo $result->numero ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Complemento</strong></td>
+                                    <td>
+                                        <?php echo $result->complemento ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Bairro</strong></td>
+                                    <td>
+                                        <?php echo $result->bairro ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>Cidade</strong></td>
+                                    <td>
+                                        <?php echo $result->cidade ?> -
+                                        <?php echo $result->estado ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right"><strong>CEP</strong></td>
+                                    <td>
+                                        <?php echo $result->cep ?>
+                                    </td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 </div>
             </div>
-
-
-
-
         </div>
-
-
         <!--Tab 2-->
         <div id="tab2" class="tab-pane" style="min-height: 300px">
             <?php if (!$results) { ?>
-
                 <table class="table table-bordered ">
                     <thead>
-                        <tr style="backgroud-color: #2D335B">
-                            <th>N° OS</th>
-                            <th>Data Inicial</th>
-                            <th>Data Final</th>
-                            <th>Descricao</th>
-                            <th>Defeito</th>
-                            <th></th>
-                        </tr>
+                    <tr style="backgroud-color: #2D335B">
+                        <th>N° OS</th>
+                        <th>Data Inicial</th>
+                        <th>Data Final</th>
+                        <th>Descricao</th>
+                        <th>Defeito</th>
+                        <th></th>
+                    </tr>
                     </thead>
                     <tbody>
-
-                        <tr>
-                            <td colspan="6">Nenhuma OS Cadastrada</td>
-                        </tr>
+                    <tr>
+                        <td colspan="6">Nenhuma OS Cadastrada</td>
+                    </tr>
                     </tbody>
                 </table>
-
-            <?php
+                <?php
             } else { ?>
-
-
-
-
                 <table class="table table-bordered ">
                     <thead>
-                        <tr style="backgroud-color: #2D335B">
-                            <th>N° OS</th>
-                            <th>Data Inicial</th>
-                            <th>Data Final</th>
-                            <th>Descricao</th>
-                            <th>Defeito</th>
-                            <th></th>
-                        </tr>
+                    <tr style="backgroud-color: #2D335B">
+                        <th>N° OS</th>
+                        <th>Data Inicial</th>
+                        <th>Data Final</th>
+                        <th>Descricao</th>
+                        <th>Defeito</th>
+                        <th></th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <?php
-                            foreach ($results as $r) {
-                                $dataInicial = date(('d/m/Y'), strtotime($r->dataInicial));
-                                $dataFinal = date(('d/m/Y'), strtotime($r->dataFinal));
-                                echo '<tr>';
-                                echo '<td>' . $r->idOs . '</td>';
-                                echo '<td>' . $dataInicial . '</td>';
-                                echo '<td>' . $dataFinal . '</td>';
-                                echo '<td>' . $r->descricaoProduto . '</td>';
-                                echo '<td>' . $r->defeito . '</td>';
+                    <?php
+                    foreach ($results as $r) {
+                        $dataInicial = date(('d/m/Y'), strtotime($r->dataInicial));
+                        $dataFinal = date(('d/m/Y'), strtotime($r->dataFinal));
+                        echo '<tr>';
+                        echo '<td>' . $r->idOs . '</td>';
+                        echo '<td>' . $dataInicial . '</td>';
+                        echo '<td>' . $dataFinal . '</td>';
+                        echo '<td>' . $r->descricaoProduto . '</td>';
+                        echo '<td>' . $r->defeito . '</td>';
 
-                                echo '<td>';
-                                if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vOs')) {
-                                    echo '<a href="' . base_url() . 'index.php/os/visualizar/' . $r->idOs . '" style="margin-right: 1%" class="btn tip-top" title="Ver mais detalhes"><i class="fas fa-eye"></i></a>';
-                                }
-                                if ($this->permission->checkPermission($this->session->userdata('permissao'), 'eOs')) {
-                                    echo '<a href="' . base_url() . 'index.php/os/editar/' . $r->idOs . '" class="btn btn-info tip-top" title="Editar OS"><i class="fas fa-edit"></i></a>';
-                                }
+                        echo '<td>';
+                        if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vOs')) {
+                            echo '<a href="' . base_url() . 'index.php/os/visualizar/' . $r->idOs . '" style="margin-right: 1%" class="btn tip-top" title="Ver mais detalhes"><i class="fas fa-eye"></i></a>';
+                        }
+                        if ($this->permission->checkPermission($this->session->userdata('permissao'), 'eOs')) {
+                            echo '<a href="' . base_url() . 'index.php/os/editar/' . $r->idOs . '" class="btn btn-info tip-top" title="Editar OS"><i class="fas fa-edit"></i></a>';
+                        }
 
-                                echo  '</td>';
-                                echo '</tr>';
-                            } ?>
-                        <tr>
-
-                        </tr>
+                        echo  '</td>';
+                        echo '</tr>';
+                    } ?>
+                    <tr>
+                    </tr>
                     </tbody>
                 </table>
-
-
-            <?php
+                <?php
             } ?>
-
         </div>
     </div>
 </div>
