72583bd1e273950c97bfa9c901084b9931ec920f
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-01-27 09:57:25 -0300

    fix: adiciona o focus do formulario adicionar cliente no campo nome

--- application/views/clientes/adicionarCliente.php
+++ application/views/clientes/adicionarCliente.php
@@ -1,25 +1,28 @@
-<script src="<?php echo base_url()?>assets/js/jquery.mask.min.js"></script>
-<script src="<?php echo base_url()?>assets/js/sweetalert2.all.min.js"></script>
-<script src="<?php echo base_url()?>assets/js/funcoes.js"></script>
+<script src="<?php echo base_url() ?>assets/js/jquery.mask.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/sweetalert2.all.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/funcoes.js"></script>
 <style>
     /* Hiding the checkbox, but allowing it to be focused */
     .badgebox {
         opacity: 0;
     }
-    .badgebox+.badge {
+
+    .badgebox + .badge {
         /* Move the check mark away when unchecked */
         text-indent: -999999px;
         /* Makes the badge's width stay the same checked and unchecked */
         width: 27px;
     }
-    .badgebox:focus+.badge {
+
+    .badgebox:focus + .badge {
         /* Set something to make the badge looks focused */
         /* This really depends on the application, in my case it was: */
         /* Adding a light border */
         box-shadow: inset 0px 0px 5px;
         /* Taking the difference out of the padding */
     }
-    .badgebox:checked+.badge {
+
+    .badgebox:checked + .badge {
         /* Move the check mark back when checked */
         text-indent: 0;
     }
@@ -33,7 +36,8 @@
             </span>
                 <h5>Cadastro de Cliente</h5>
                 <div class="buttons">
-                    <a title="Voltar" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/clientes"><i class="fas fa-arrow-left"></i> Voltar</a>
+                    <a title="Voltar" class="btn btn-mini btn-inverse" href="<?php echo site_url() ?>/clientes"><i
+                                class="fas fa-arrow-left"></i> Voltar</a>
                 </div>
             </div>
             <ul class="nav nav-tabs">
@@ -49,38 +53,46 @@
                         <div class="control-group">
                             <label for="documento" class="control-label">CPF/CNPJ</label>
                             <div class="controls">
-                                <input id="documento" class="cpfcnpj" type="text" name="documento" value="<?php echo set_value('documento'); ?>"  />
-                                <button id="buscar_info_cnpj" class="btn btn-xs" type="button">Buscar Informações (CNPJ)</button>
+                                <input id="documento" class="cpfcnpj" type="text" name="documento"
+                                       value="<?php echo set_value('documento'); ?>"/>
+                                <button id="buscar_info_cnpj" class="btn btn-xs" type="button">Buscar Informações
+                                    (CNPJ)
+                                </button>
                             </div>
                         </div>
                         <div class="control-group">
-                            <label for="nomeCliente" class="control-label">Nome/Razão Social<span class="required">*</span></label>
+                            <label for="nomeCliente" class="control-label">Nome/Razão Social<span
+                                        class="required">*</span></label>
                             <div class="controls">
-                                <input id="nomeCliente" type="text" name="nomeCliente" value="<?php echo set_value('nomeCliente'); ?>" />
+                                <input id="nomeCliente" type="text" name="nomeCliente"
+                                       value="<?php echo set_value('nomeCliente'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="contato" class="control-label">Contato:</label>
                             <div class="controls">
-                                <input class="nomeCliente" type="text" name="contato" value="<?php echo set_value('contato'); ?>" />
+                                <input class="nomeCliente" type="text" name="contato"
+                                       value="<?php echo set_value('contato'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="telefone" class="control-label">Telefone</label>
                             <div class="controls">
-                                <input id="telefone" type="text" name="telefone" value="<?php echo set_value('telefone'); ?>" />
+                                <input id="telefone" type="text" name="telefone"
+                                       value="<?php echo set_value('telefone'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="celular" class="control-label">Celular</label>
                             <div class="controls">
-                                <input id="celular" type="text" name="celular" value="<?php echo set_value('celular'); ?>" />
+                                <input id="celular" type="text" name="celular"
+                                       value="<?php echo set_value('celular'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="email" class="control-label">Email</label>
                             <div class="controls">
-                                <input id="email" type="text" name="email" value="<?php echo set_value('email'); ?>" />
+                                <input id="email" type="text" name="email" value="<?php echo set_value('email'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
@@ -105,37 +117,41 @@
                         <div class="control-group" class="control-label">
                             <label for="cep" class="control-label">CEP</label>
                             <div class="controls">
-                                <input id="cep" type="text" name="cep" value="<?php echo set_value('cep'); ?>" />
+                                <input id="cep" type="text" name="cep" value="<?php echo set_value('cep'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
                             <label for="rua" class="control-label">Rua</label>
                             <div class="controls">
-                                <input id="rua" type="text" name="rua" value="<?php echo set_value('rua'); ?>" />
+                                <input id="rua" type="text" name="rua" value="<?php echo set_value('rua'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="numero" class="control-label">Número</label>
                             <div class="controls">
-                                <input id="numero" type="text" name="numero" value="<?php echo set_value('numero'); ?>" />
+                                <input id="numero" type="text" name="numero"
+                                       value="<?php echo set_value('numero'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group">
                             <label for="complemento" class="control-label">Complemento</label>
                             <div class="controls">
-                                <input id="complemento" type="text" name="complemento" value="<?php echo set_value('complemento'); ?>" />
+                                <input id="complemento" type="text" name="complemento"
+                                       value="<?php echo set_value('complemento'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
                             <label for="bairro" class="control-label">Bairro</label>
                             <div class="controls">
-                                <input id="bairro" type="text" name="bairro" value="<?php echo set_value('bairro'); ?>" />
+                                <input id="bairro" type="text" name="bairro"
+                                       value="<?php echo set_value('bairro'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
                             <label for="cidade" class="control-label">Cidade</label>
                             <div class="controls">
-                                <input id="cidade" type="text" name="cidade" value="<?php echo set_value('cidade'); ?>" />
+                                <input id="cidade" type="text" name="cidade"
+                                       value="<?php echo set_value('cidade'); ?>"/>
                             </div>
                         </div>
                         <div class="control-group" class="control-label">
@@ -161,14 +177,17 @@
 </div>
 <script src="<?php echo base_url() ?>assets/js/jquery.validate.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $.getJSON('<?php echo base_url() ?>assets/json/estados.json', function(data) {
+    $(document).ready(function () {
+        $.getJSON('<?php echo base_url() ?>assets/json/estados.json', function (data) {
             for (i in data.estados) {
                 $('#estado').append(new Option(data.estados[i].nome, data.estados[i].sigla));
                 var curState = '<?php echo set_value('estado'); ?>';
-                if (curState) { $("#estado option[value=" + curState + "]").prop("selected",true); }
+                if (curState) {
+                    $("#estado option[value=" + curState + "]").prop("selected", true);
+                }
             }
         });
+        $("#nomeCliente").focus();
         $('#formCliente').validate({
             rules: {
                 nomeCliente: {
@@ -183,10 +202,10 @@
 
             errorClass: "help-inline",
             errorElement: "span",
-            highlight: function(element, errorClass, validClass) {
+            highlight: function (element, errorClass, validClass) {
                 $(element).parents('.control-group').addClass('error');
             },
-            unhighlight: function(element, errorClass, validClass) {
+            unhighlight: function (element, errorClass, validClass) {
                 $(element).parents('.control-group').removeClass('error');
                 $(element).parents('.control-group').addClass('success');
             }
