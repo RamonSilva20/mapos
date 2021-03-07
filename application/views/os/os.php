79309e92c441284d836b2c391d16e62fef4fa19b
Bulfaitelo <Bulfaitelo@gmail.com>
2021-03-07 18:40:49 -0300

    Melhorando Lógica de validação
    Obrigando a selecionar uma das opções!

--- application/views/os/os.php
+++ application/views/os/os.php
@@ -80,9 +80,11 @@
                             $dataFinal = date(('d/m/Y'), strtotime($r->dataFinal));
                         } else {
                             $dataFinal = "";
-                        }
-                        if ($this->input->get('pesquisa') !== '' && in_array($r->status, json_decode($configuration['os_status_list'])) != true ) {
-                            continue;
+                        }                       
+                        if ($this->input->get('pesquisa') === null && is_array(json_decode($configuration['os_status_list']))) {
+                            if(in_array($r->status, json_decode($configuration['os_status_list'])) != true){                                
+                                continue;
+                            }
                         }
 
                         switch ($r->status) {
