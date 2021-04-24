6c67373d46854859b6e47f17082990a258379460
hoshikawakun <puffy.a@hotmail.com>
2021-04-24 04:39:57 -0300

    Update imprimirClientes.php

--- application/views/relatorios/imprimir/imprimirClientes.php
+++ application/views/relatorios/imprimir/imprimirClientes.php
@@ -2,8 +2,7 @@
 <html>
 
 <head>
-    <title>MAPOS - <?= $title ?>
-    </title>
+    <title>MAPOS</title>
     <meta charset="UTF-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -21,52 +20,42 @@
                 <div class="widget-box">
                     <?= $topo ?>
                     <div class="widget-title">
-                        <h4 style="text-align: center">
-                            <?= $title ?>
+                        <h4 style="text-align: center; font-size: 1.1em; padding: 5px;">
+                            <?= ucfirst($title) ?>
                         </h4>
-                        <br>
                     </div>
-                    <div class="widget-content nopadding tab-content">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th style="font-size: 1.2em; padding: 5px;">Nome</th>
-                                    <th style="font-size: 1.2em; padding: 5px;">Documento</th>
-                                    <th style="font-size: 1.2em; padding: 5px;">Telefone</th>
-                                    <th style="font-size: 1.2em; padding: 5px;">Email</th>
-                                    <th style="font-size: 1.2em; padding: 5px;">Cadastro</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($clientes as $c) : ?>
-                                <?php $dataCadastro = date('d/m/Y', strtotime($c->dataCadastro)) ?>
-                                <tr>
-                                    <td>
-                                        <?= $c->nomeCliente ?>
-                                    </td>
-                                    <td>
-                                        <?= $c->documento ?>
-                                    </td>
-                                    <td>
-                                        <?= $c->telefone ?>
-                                    </td>
-                                    <td>
-                                        <?= $c->email ?>
-                                    </td>
-                                    <td>
-                                        <?= $dataCadastro ?>
-                                    </td>
-                                </tr>
-                                <?php endforeach ?>
-                            </tbody>
-                        </table>
+                        <div class="widget-content nopadding tab-content">
+                        <table width="100%" class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th width="480" style="font-size: 15px">Nome</th>
+                                        <th width="170" style="font-size: 15px">Documento</th>
+                                        <th width="150" style="font-size: 15px">Telefone</th>
+                                        <th width="200" style="font-size: 15px">Email</th>
+                                        <th width="120" style="font-size: 15px">Cadastro</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($clientes as $c) : ?>
+                                    <?php $dataCadastro = date('d/m/Y', strtotime($c->dataCadastro)) ?>
+                                    <tr>
+                                        <td><?= $c->nomeCliente ?></td>
+                                        <td align="center"><?= $c->documento ?></td>
+                                        <td align="center"><?= $c->telefone ?></td>
+                                        <td align="center"><?= $c->email ?></td>
+                                        <td align="center"><?= $dataCadastro ?></td>
+                                    </tr>
+                                    <?php endforeach ?>
+                                </tbody>
+                            </table>
 
-                    </div>
+                        </div>
+
                 </div>
-
-                <h5 style="text-align: right">Data do Relatório:
+                <h5 style="text-align: right; font-size: 0.8em; padding: 5px;">Data do Relatório:
                     <?php echo date('d/m/Y'); ?>
                 </h5>
+
             </div>
         </div>
     </div>
