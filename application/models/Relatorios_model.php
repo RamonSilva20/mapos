51ed4242fe7f5570a567257d6ba6183b6108da6d
hoshikawakun <puffy.a@hotmail.com>
2021-04-24 04:39:54 -0300

    Update Relatorios_model.php

--- application/models/Relatorios_model.php
+++ application/models/Relatorios_model.php
@@ -339,12 +339,12 @@ class Relatorios_model extends CI_Model
 
     public function vendasRapid($array = false)
     {
-        $this->db->select('vendas.*,clientes.nomeCliente, usuarios.nome,itens_de_vendas.subTotal as valorTotal, itens_de_vendas.vendas_id');
+        $this->db->select('vendas.*,clientes.nomeCliente, usuarios.nome'); //,itens_de_vendas.subTotal as valorTotal, itens_de_vendas.vendas_id');
         $this->db->from('vendas');
         $this->db->join('clientes', 'clientes.idClientes = vendas.clientes_id');
         $this->db->join('usuarios', 'usuarios.idUsuarios = vendas.usuarios_id');
-        $this->db->join('itens_de_vendas', 'itens_de_vendas.vendas_id = vendas.idVendas');
-        $this->db->order_by('vendas.dataVenda', 'DESC');
+        //$this->db->join('itens_de_vendas', 'itens_de_vendas.vendas_id = vendas.idVendas');
+        $this->db->order_by('vendas.idVendas', 'ASC');
 
         $result = $this->db->get();
         if ($array) {
@@ -373,11 +373,16 @@ class Relatorios_model extends CI_Model
             $whereResponsavel = "AND usuarios_id = " . $this->db->escape($responsavel);
         }
 
-        $query = "SELECT vendas.*,clientes.nomeCliente, usuarios.nome,itens_de_vendas.subTotal as valorTotal, itens_de_vendas.vendas_id FROM vendas
+        $query = "SELECT vendas.*,clientes.nomeCliente, usuarios.nome FROM vendas
         LEFT JOIN clientes ON vendas.clientes_id = clientes.idClientes
         LEFT JOIN usuarios ON vendas.usuarios_id = usuarios.idUsuarios
-        LEFT JOIN itens_de_vendas ON itens_de_vendas.vendas_id = vendas.idVendas
-        WHERE idVendas != 0 $whereData $whereCliente $whereResponsavel ORDER BY vendas.dataVenda";
+        WHERE idVendas != 0 $whereData $whereCliente $whereResponsavel ORDER BY vendas.idVendas";
+		
+		//$query = "SELECT vendas.*,clientes.nomeCliente, usuarios.nome,itens_de_vendas.subTotal as valorTotal, itens_de_vendas.vendas_id FROM vendas
+        //LEFT JOIN clientes ON vendas.clientes_id = clientes.idClientes
+       // LEFT JOIN usuarios ON vendas.usuarios_id = usuarios.idUsuarios
+       // LEFT JOIN itens_de_vendas ON itens_de_vendas.vendas_id = vendas.idVendas
+       // WHERE idVendas != 0 $whereData $whereCliente $whereResponsavel ORDER BY vendas.dataVenda";
 
         $result = $this->db->query($query);
         if ($array) {
