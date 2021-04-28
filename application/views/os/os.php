accd14e639e34b62d68898944782b1f8e77ab7d0
Franklin <55247088+fwsund@users.noreply.github.com>
2021-04-28 20:08:45 -0300

    Revert "Adicionar opção de status Aprovado"
    This reverts commit 15c464470daae0ab30d6fbcd6252396d21d78219.

--- application/views/os/os.php
+++ application/views/os/os.php
@@ -26,7 +26,6 @@
                 <option value="Finalizado">Finalizado</option>
                 <option value="Cancelado">Cancelado</option>
                 <option value="Aguardando Peças">Aguardando Peças</option>
-				<option value="Aprovado">Aprovado</option>
             </select>
 
         </div>
@@ -112,9 +111,6 @@
                                 break;
                             case 'Aguardando Peças':
                                 $cor = '#FF7F00';
-                                break;
-								case 'Aprovado':
-                                $cor = '#808080';
                                 break;
                             default:
                                 $cor = '#E0E4CC';
