a45253b9e756d6fd167c0be263ba26f2aae179ef
Gianluca Bine <gian_bine@hotmail.com>
2021-05-22 14:49:27 -0300

    fix: atualizado versão de php cs fixer e arquivos de configuração

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,11 +8,11 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->name('*.php')
     ->notName('*.blade.php');
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
     ])
     ->setFinder($finder);
