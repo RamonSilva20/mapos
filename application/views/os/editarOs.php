6e5de17ff58b8c3ca6f0da92eeb213315e3af565
Gianluca Bine <gian_bine@hotmail.com>
2021-10-02 15:30:26 -0300

    fix: correção de bug que faturava OS/Venda incorretamente e controle de edição de OS
    * fix: correção de bug que faturava OS/Venda incorretamente com valores superiores a R$ 1000,00
    
    * fix: corrigido configuração de controle de edição de OS

--- application/views/os/editarOs.php
+++ application/views/os/editarOs.php
@@ -465,7 +465,7 @@
                 <div class="span5" style="margin-left: 0">
                     <label for="valor">Valor*</label>
                     <input type="hidden" id="tipo" name="tipo" value="receita" />
-                    <input class="span12 money" id="valor" type="text" data-affixes-stay="true" data-thousands="" data-decimal="." name="valor" value="<?php echo number_format($totals + $total, 2); ?>" />
+                    <input class="span12 money" id="valor" type="text" data-affixes-stay="true" data-thousands="" data-decimal="." name="valor" value="<?php echo number_format($totals + $total, 2, '.', ''); ?>" />
                     <strong><span style="color: red" id="resultado"></span></strong>
                 </div>
                 <div class="span4">
