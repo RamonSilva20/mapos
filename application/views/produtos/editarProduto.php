05aa3af88cb2c4761896fe9e5bf1ac944b1e2057
Edmison Elias <92489803+MilsonElias@users.noreply.github.com>
2022-02-07 01:26:53 -0300

    Update editarProduto.php
    Icon voltar modificado

--- application/views/produtos/editarProduto.php
+++ application/views/produtos/editarProduto.php
@@ -106,7 +106,7 @@
                         <div class="span12">
                             <div class="span6 offset3">
                                 <button type="submit" class="btn btn-primary"><i class="fas fa-sync-alt"></i> Atualizar</button>
-                                <a href="<?php echo base_url() ?>index.php/produtos" id="" class="btn"><i class="fas fa-backward"></i> Voltar</a>
+                                <a href="<?php echo base_url() ?>index.php/produtos" id="" class="btn"><i class="fas fa-arrow-left"></i> Voltar</a>
                             </div>
                         </div>
                     </div>
