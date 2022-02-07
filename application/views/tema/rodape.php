f896d8f10d11780045187ba7fb1cc698353d9a41
Edmison Elias <92489803+MilsonElias@users.noreply.github.com>
2022-02-07 01:36:40 -0300

    Update rodape.php
    Titulos modificados e adicionado versão de sistema

--- application/views/tema/rodape.php
+++ application/views/tema/rodape.php
@@ -1,7 +1,7 @@
-<!--Footer-part-->
 <div class="row-fluid">
-    <div id="footer" class="span12"> <a href="https://github.com/RamonSilva20/mapos" target="_blank">
-            <?= date('Y'); ?> &copy; Map-OS - Ramon Silva </a></div>
+    <div id="footer" class="span12"> <a class="pecolor" href="https://github.com/RamonSilva20/mapos" target="_blank">
+            <?= date('Y'); ?> &copy; Ramon Silva - Map-OS - Versão:
+      <?= $this->config->item('app_version'); ?></a></div>
 </div>
 <!--end-Footer-part-->
 <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
