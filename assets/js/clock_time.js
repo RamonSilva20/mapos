374573c9fb15633cce1f5b06b55f910621187ff8
Edmison Elias <92489803+MilsonElias@users.noreply.github.com>
2022-02-12 22:13:12 -0300

    Segundos removidos

--- assets/js/clock_time.js
+++ assets/js/clock_time.js
@@ -3,8 +3,8 @@ setInterval(() => {
 
     let date = new Date(),
         hour = date.getHours(),
-        min = date.getMinutes(),
-        sec = date.getSeconds();
+        min = date.getMinutes();
+    // sec = date.getSeconds();
 
     let d;
     d = hour < 12 ? "AM" : "PM";
@@ -14,13 +14,13 @@ setInterval(() => {
     // adicionar 0 a esquerda das unidades Exp: h:m:s antes 2:5:7, depois 02:05:07
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
+    // sec = sec < 10 ? "0" + sec : sec;
 
     console.log(min);
 
     document.querySelector(".hour_num").innerText = hour;
     document.querySelector(".min_num").innerText = min;
-    document.querySelector(".sec_num").innerText = sec;
+    // document.querySelector(".sec_num").innerText = sec;
     document.querySelector(".am_pm").innerText = d;
 
 }, 1000); // 1000 milésimos é que vale a 1 segundo
