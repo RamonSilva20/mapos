faabe79e8befa423c65cc8ca0c6968c9f9508893
Edmison Elias <92489803+MilsonElias@users.noreply.github.com>
2022-02-17 00:40:41 -0300

    styles

--- application/views/tema/topo.php
+++ application/views/tema/topo.php
@@ -104,7 +104,7 @@
   </div>
 
 <!-- New User -->
-<div id="userr" style="padding-right:48px;display:flex;flex-direction:column;align-items:flex-end;justify-content:center;">
+<div id="userr" style="padding-right:45px;display:flex;flex-direction:column;align-items:flex-end;justify-content:center;">
   <div class="user-names userT0"> <?php function saudacao( $nome = '' ) { date_default_timezone_set('America/Sao_Paulo'); $hora = date('H');
       if( $hora >= 6 && $hora <= 12 ) return 'Bom dia' . (empty($nome) ? '' : ', ' . $nome);
       else if ( $hora > 12 && $hora <=18  ) return 'Boa tarde' . (empty($nome) ? '' : ', ' . $nome);
