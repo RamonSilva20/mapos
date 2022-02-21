6c2b3b68ab1ead4ae23c3a9578a8c14a4f7ba8b1
Edmison Elias <92489803+MilsonElias@users.noreply.github.com>
2022-02-21 08:41:37 -0300

    Botões da modal do painel do usuário que restavam

--- application/views/mapos/painel.php
+++ application/views/mapos/painel.php
@@ -688,9 +688,9 @@
             <input type="hidden" id="modalIdExcluir" name="id" value="" />
             <h5 style="text-align: center">Deseja realmente excluir esta OS?</h5>
         </div>
-        <div class="modal-footer">
-            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
-            <button class="btn btn-danger">Excluir</button>
+        <div class="modal-footer" style="display:flex;justify-content: center">
+          <button class="button btn btn-warning" data-dismiss="modal" aria-hidden="true"><span class="button__icon"><i class="bx bx-x"></i></span><span class="button__text2">Cancelar</span></button>
+          <button class="button btn btn-danger"><span class="button__icon"><i class='bx bx-trash'></i></span> <span class="button__text2">Excluir</span></button>
         </div>
     </form>
 </div>
@@ -718,9 +718,9 @@
                 </div>
             </div>
         </div>
-        <div class="modal-footer">
-            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
-            <button class="btn btn-primary">Atualizar</button>
+        <div class="modal-footer" style="display:flex;justify-content: center">
+            <button class="button btn btn-warning" data-dismiss="modal" aria-hidden="true"><span class="button__icon"><i class="bx bx-x"></i></span><span class="button__text2">Cancelar</span></button>
+            <button class="btn btn-primary"><span class="button__icon"><i class="bx bx-sync"></i></span><span class="button__text2">Atualizar</span></button>
         </div>
     </form>
 </div>
