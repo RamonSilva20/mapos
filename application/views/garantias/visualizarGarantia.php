4671add720d513d094d6e32533baab228c0f3f70
willph <willph@users.noreply.github.com>
2022-02-23 22:04:55 +0000

    Apply php-cs-fixer changes

--- application/views/garantias/visualizarGarantia.php
+++ application/views/garantias/visualizarGarantia.php
@@ -11,7 +11,7 @@
                     <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'eGarantia')) {
     echo '<a title="Editar Termo de Garantia" class="button btn btn-mini btn-success" href="' . base_url() . 'index.php/garantias/editar/' . $result->idGarantias . '">
     <span class="button__icon"><i class="bx bx-edit"></i> </span> <span class="button__text">Editar</span></a>';
-  } ?>
+} ?>
                     <a target="_blank" title="Imprimir" class="button btn btn-mini btn-inverse" href="<?php echo site_url() ?>/garantias/imprimir/<?php echo $result->idGarantias; ?>">
                       <span class="button__icon"><i class="bx bx-printer"></i></span> <span class="button__text">Imprimir</span></a>
                 </div>
