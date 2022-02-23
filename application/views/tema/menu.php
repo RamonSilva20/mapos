46f5c443a307944a1d10fc8102d70d5025ba88a8
willph <willph@users.noreply.github.com>
2022-02-23 14:58:29 +0000

    Apply php-cs-fixer changes

--- application/views/tema/menu.php
+++ application/views/tema/menu.php
@@ -31,16 +31,16 @@
 
             <ul class="menu-links" style="position: relative;">
                 <li class="<?php if (isset($menuPainel)) {
-                                echo 'active';
-                            }; ?>">
+    echo 'active';
+}; ?>">
                     <a href="<?= base_url() ?>"><i class='bx bx-home-alt iconX'></i>
                         <span class="title nav-title">Início</span></a>
                 </li>
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vCliente')) { ?>
                     <li class="<?php if (isset($menuClientes)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('clientes') ?>"><i class='bx bx-group iconX'></i>
                             <span class="title">Cliente / Fornecedor</span></a>
                     </li>
@@ -48,8 +48,8 @@
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vProduto')) { ?>
                     <li class="<?php if (isset($menuProdutos)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('produtos') ?>"><i class='bx bx-package iconX'></i>
                             <span class="title">Produtos</span></a>
                     </li>
@@ -57,8 +57,8 @@
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vServico')) { ?>
                     <li class="<?php if (isset($menuServicos)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('servicos') ?>"><i class='bx bx-stopwatch iconX'></i>
                             <span class="title">Serviços</span></a>
                     </li>
@@ -66,8 +66,8 @@
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vVenda')) { ?>
                     <li class="<?php if (isset($menuVendas)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('vendas') ?>"><i class='bx bx-cart-alt iconX'></i></span>
                             <span class="title">Vendas</span></a>
                     </li>
@@ -75,8 +75,8 @@
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vOs')) { ?>
                     <li class="<?php if (isset($menuOs)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('os') ?>"><i class='bx bx-spreadsheet iconX'></i>
                             <span class="title">Ordens de Serviço</span></a>
                     </li>
@@ -84,8 +84,8 @@
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vGarantia')) { ?>
                     <li class="<?php if (isset($menuGarantia)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('garantias') ?>"><i class='bx bx-receipt iconX'></i>
                             <span class="title">Termos de Garantias</span></a>
                     </li>
@@ -93,8 +93,8 @@
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vArquivo')) { ?>
                     <li class="<?php if (isset($menuArquivos)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('arquivos') ?>"><i class='bx bx-box iconX'></i>
                             <span class="title">Arquivos</span></a>
                     </li>
@@ -102,16 +102,16 @@
 
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vLancamento')) { ?>
                     <li class="<?php if (isset($menuLancamentos)) {
-                                    echo 'active';
-                                }; ?>">
+    echo 'active';
+}; ?>">
                         <a href="<?= site_url('financeiro/lancamentos') ?>"><i class="bx bx-bar-chart-square iconX"></i>
                             <span class="title">Lançamentos</span></a>
                     </li>
                 <?php } ?>
                 <?php if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vLancamento')) { ?>
                 <li class="<?php if (isset($menuCobrancas)) {
-                                echo 'active';
-                            }; ?>">
+    echo 'active';
+}; ?>">
                     <a href="<?= site_url('cobrancas/cobrancas') ?>"><i class='bx bx-credit-card-front iconX'></i>
                         <span class="title">Cobranças</span></a>
                 </li>
