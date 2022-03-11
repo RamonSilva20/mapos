beb2c96e763d6836223ad9221b55b9fc5591b5d3
Wilmerson <will.phelipe@gmail.com>
2022-03-11 19:52:51 -0300

    bug fix sql create password

--- application/database/migrations/20220307173741_add_password_client.php
+++ application/database/migrations/20220307173741_add_password_client.php
@@ -4,19 +4,22 @@ class Migration_add_password_client extends CI_Migration
 {
     public function up()
     {
-        $sql = "
-            ALTER TABLE `clientes` ADD `senha` VARCHAR(200) NOT NULL ;
-            CREATE TABLE `resets_de_senha` ( `id` INT(11) NOT NULL AUTO_INCREMENT = 1, add PRIMARY KEY (`id`)  , `email` VARCHAR(200) NOT NULL , `token` VARCHAR(255) NOT NULL , `data_expiracao` DATETIME NOT NULL, `token_utilizado` TINYINT NOT NULL ) ENGINE = InnoDB; 
-        ";
-        $this->db->query($sql);
+        $this->db->query("ALTER TABLE `clientes` ADD `senha` VARCHAR(200) NOT NULL;");
+        $this->db->query("CREATE TABLE `resets_de_senha` ( 
+                `id` INT NOT NULL AUTO_INCREMENT,
+                `email` VARCHAR(200) NOT NULL , 
+                `token` VARCHAR(255) NOT NULL , 
+                `data_expiracao` DATETIME NOT NULL, 
+                `token_utilizado` TINYINT NOT NULL,
+                PRIMARY KEY (`id`))
+              ENGINE = InnoDB
+              DEFAULT CHARACTER SET = latin1;
+        ");
     }
 
     public function down()
     {
-        $sql = "
-            ALTER TABLE `clientes` DROP COLUMN `senha`;
-            DROP TABLE IF EXISTS `resets_de_senha`;
-        ";
-        $this->db->query($sql);
+        $this->db->query("ALTER TABLE `clientes` DROP `senha`;");
+        $this->db->query("DROP TABLE `resets_de_senha`;");
     }
 }
--- updates/update_4.36.0_to_4.37.0.sql
+++ updates/update_4.36.0_to_4.37.0.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `clientes` ADD `senha` VARCHAR(200) NOT NULL ;
-CREATE TABLE `resets_de_senha` ( `id` INT(11) NOT NULL AUTO_INCREMENT = 1, add PRIMARY KEY (`id`)  , `email` VARCHAR(200) NOT NULL , `token` VARCHAR(255) NOT NULL , `data_expiracao` DATETIME NOT NULL, `token_utilizado` TINYINT NOT NULL ) ENGINE = InnoDB;
+ALTER TABLE `clientes` ADD `senha` VARCHAR(200) NOT NULL;
+CREATE TABLE `resets_de_senha` ( `id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(200) NOT NULL , `token` VARCHAR(255) NOT NULL , `data_expiracao` DATETIME NOT NULL, `token_utilizado` TINYINT NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
