68a2fcb3d7ab7974ffed10dc1efeb116da0c8352
Wilmerson <will.phelipe@gmail.com>
2022-03-13 22:58:44 -0300

    transparency chart in painel.php

--- application/views/mapos/painel.php
+++ application/views/mapos/painel.php
@@ -288,7 +288,7 @@
                         <?php echo($financeiro_mes->VALOR_DEZ_REC - $financeiro_mes->VALOR_DEZ_DES); ?>
                     ],
 
-                    backgroundColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.5)',
                     borderRadius: 15,
                 },
 
@@ -308,7 +308,7 @@
                         <?php echo($financeiro_mes->VALOR_DEZ_REC); ?>
                     ],
 
-                    backgroundColor: 'rgba(255, 206, 86, 1)',
+                    backgroundColor: 'rgba(255, 206, 86, 0.5)',
                     borderRadius: 15,
                 },
 
@@ -328,7 +328,7 @@
                         <?php echo($financeiro_mes->VALOR_DEZ_DES); ?>
                     ],
 
-                    backgroundColor: 'rgba(255, 99, 132, 1)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
                     borderRadius: 15,
                 },
 
@@ -348,7 +348,7 @@
                         <?php echo($financeiro_mesinadipl->VALOR_DEZ_REC); ?>
                     ],
 
-                    backgroundColor: 'rgba(54, 162, 235, 1)',
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
                     borderRadius: 15,
                 }
             ]
@@ -417,12 +417,12 @@
                 ],
 
                 backgroundColor: [
-                    'rgba(75, 192, 192, 0.3)',
-                    'rgba(54, 162, 235, 0.3)',
-                    'rgba(255, 206, 86, 0.3)',
-                    'rgba(255, 99, 132, 0.3)',
-                    'rgba(255, 159, 64, 0.3)',
-                    'rgba(153, 102, 255, 0.3)'
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(255, 159, 64, 0.5)',
+                    'rgba(153, 102, 255, 0.5)'
                 ],
                 borderWidth: 1
             }]
