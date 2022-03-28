e1f5338fe205310b34d228642a0748fd9d30fe92
willph <willph@users.noreply.github.com>
2022-03-28 19:46:50 +0000

    Apply php-cs-fixer changes

--- application/views/os/editarOs.php
+++ application/views/os/editarOs.php
@@ -93,40 +93,40 @@
                                             <label for="status">Status<span class="required">*</span></label>
                                             <select class="span12" name="status" id="status" value="">
                                                 <option <?php if ($result->status == 'Orçamento') {
-                                                            echo 'selected';
-                                                        } ?> value="Orçamento">Orçamento
+                        echo 'selected';
+                    } ?> value="Orçamento">Orçamento
                                                 </option>
                                                 <option <?php if ($result->status == 'Aberto') {
-                                                            echo 'selected';
-                                                        } ?> value="Aberto">Aberto
+                        echo 'selected';
+                    } ?> value="Aberto">Aberto
                                                 </option>
                                                 <option <?php if ($result->status == 'Faturado') {
-                                                            echo 'selected';
-                                                        } ?> value="Faturado">Faturado
+                        echo 'selected';
+                    } ?> value="Faturado">Faturado
                                                 </option>
                                                 <option <?php if ($result->status == 'Negociação') {
-                                                            echo 'selected';
-                                                        } ?> value="Negociação">Negociação
+                        echo 'selected';
+                    } ?> value="Negociação">Negociação
                                                 </option>
                                                 <option <?php if ($result->status == 'Em Andamento') {
-                                                            echo 'selected';
-                                                        } ?> value="Em Andamento">Em Andamento
+                        echo 'selected';
+                    } ?> value="Em Andamento">Em Andamento
                                                 </option>
                                                 <option <?php if ($result->status == 'Finalizado') {
-                                                            echo 'selected';
-                                                        } ?> value="Finalizado">Finalizado
+                        echo 'selected';
+                    } ?> value="Finalizado">Finalizado
                                                 </option>
                                                 <option <?php if ($result->status == 'Cancelado') {
-                                                            echo 'selected';
-                                                        } ?> value="Cancelado">Cancelado
+                        echo 'selected';
+                    } ?> value="Cancelado">Cancelado
                                                 </option>
                                                 <option <?php if ($result->status == 'Aguardando Peças') {
-                                                            echo 'selected';
-                                                        } ?> value="Aguardando Peças">Aguardando Peças
+                        echo 'selected';
+                    } ?> value="Aguardando Peças">Aguardando Peças
                                                 </option>
                                                 <option <?php if ($result->status == 'Aprovado') {
-                                                            echo 'selected';
-                                                        } ?> value="Aprovado">Aprovado
+                        echo 'selected';
+                    } ?> value="Aprovado">Aprovado
                                                 </option>
                                             </select>
                                         </div>
@@ -930,8 +930,8 @@
                 var estoque = parseInt($("#estoque").val());
 
                 <?php if (!$configuration['control_estoque']) {
-                    echo 'estoque = 1000000';
-                }; ?>
+                                                echo 'estoque = 1000000';
+                                            }; ?>
 
                 if (estoque < quantidade) {
                     Swal.fire({
