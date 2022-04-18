daed1c46dd5e2f5ac22b22afbf272297771fd92e
Wilmerson <will.phelipe@gmail.com>
2022-04-18 17:49:58 -0300

    Update 20220320173741_add_desconto_lancamentos_os_vendas.php

--- application/database/migrations/20220320173741_add_desconto_lancamentos_os_vendas.php
+++ application/database/migrations/20220320173741_add_desconto_lancamentos_os_vendas.php
@@ -8,7 +8,7 @@ class Migration_add_desconto_lancamentos_os_vendas extends CI_Migration
         $this->db->query("ALTER TABLE `lancamentos` ADD `valor_desconto` DECIMAL(10, 2) NULL DEFAULT 0");
         $this->db->query("ALTER TABLE `os` ADD `desconto` DECIMAL(10, 2) NULL DEFAULT 0");
         $this->db->query("ALTER TABLE `os` ADD `valor_desconto` DECIMAL(10, 2) NULL DEFAULT 0");
-        $this->db->query("ALTER TABLE `vendas` ADD `desconto` DECIMAL(10, 2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `vendas` CHANGE `desconto` `desconto` DECIMAL(10,2) NULL DEFAULT '0.00'");
         $this->db->query("ALTER TABLE `vendas` ADD `valor_desconto` DECIMAL(10, 2) NULL DEFAULT 0");
         $this->db->query("INSERT INTO `configuracoes` (`idConfig`, `config`, `valor`) VALUES (14, 'email_automatico', 1)");
     }
