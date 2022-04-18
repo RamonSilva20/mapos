b517c3ec379b4b5cda549c52e25fc5267d946fd3
Wilmerson <will.phelipe@gmail.com>
2022-04-18 17:52:06 -0300

    Update update_4.37.0_to_4.38.0.sql

--- updates/update_4.37.0_to_4.38.0.sql
+++ updates/update_4.37.0_to_4.38.0.sql
@@ -1,7 +1,6 @@
 ALTER TABLE `os` ADD `desconto` DECIMAL(10, 2) NULL DEFAULT 0;
 ALTER TABLE `os` ADD `valor_desconto` DECIMAL(10, 2) NULL DEFAULT 0;
-ALTER TABLE `vendas` DROP `desconto`;
-ALTER TABLE `vendas` ADD `desconto` DECIMAL(10, 2) NULL DEFAULT 0;
+ALTER TABLE `vendas` CHANGE `desconto` `desconto` DECIMAL(10,2) NULL DEFAULT '0.00';
 ALTER TABLE `vendas` ADD `valor_desconto` DECIMAL(10, 2) NULL DEFAULT 0;
 ALTER TABLE `lancamentos` ADD `desconto` DECIMAL(10, 2) NULL DEFAULT 0;
 ALTER TABLE `lancamentos` ADD `valor_desconto` DECIMAL(10, 2) NULL DEFAULT 0;
