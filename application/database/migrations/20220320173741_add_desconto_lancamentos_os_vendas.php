f7f58e665f700d28654fe6fb62ba8c683d9311dc
Wilmerson <will.phelipe@gmail.com>
2022-04-18 19:17:37 -0300

    demais campos mencionado pelo Marllon

--- application/database/migrations/20220320173741_add_desconto_lancamentos_os_vendas.php
+++ application/database/migrations/20220320173741_add_desconto_lancamentos_os_vendas.php
@@ -13,6 +13,13 @@ class Migration_add_desconto_lancamentos_os_vendas extends CI_Migration
         $this->db->query("ALTER TABLE `vendas` CHANGE `valorTotal` `valorTotal` DECIMAL(10,2) NULL DEFAULT 0");
         $this->db->query("ALTER TABLE `vendas` CHANGE `desconto` `desconto` DECIMAL(10,2) NULL DEFAULT 0");
         $this->db->query("ALTER TABLE `vendas` ADD `valor_desconto` DECIMAL(10, 2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `cobrancas` CHANGE `total` `total` DECIMAL(10,2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `produtos_os` CHANGE `preco` `preco` DECIMAL(10,2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `produtos_os` CHANGE `subTotal` `subTotal` DECIMAL(10,2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `servicos_os` CHANGE `preco` `preco` DECIMAL(10,2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `servicos_os` CHANGE `subTotal` `subTotal` DECIMAL(10,2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `itens_de_vendas` CHANGE `subTotal` `subTotal` DECIMAL(10,2) NULL DEFAULT 0");
+        $this->db->query("ALTER TABLE `itens_de_vendas` CHANGE `preco` `preco` DECIMAL(10,2) NULL DEFAULT 0");
         $this->db->query("INSERT INTO `configuracoes` (`idConfig`, `config`, `valor`) VALUES (14, 'email_automatico', 1)");
     }
 
