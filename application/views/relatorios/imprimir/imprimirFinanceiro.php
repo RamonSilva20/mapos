3ee5167db63c3e60dc653b808f78ed72c90216e4
Luiz Fernando Riva Nekel <luizfernandorivanekel@hotmail.com>
2022-04-18 19:10:36 -0400

    fix relatorio data vazia

--- application/views/relatorios/imprimir/imprimirFinanceiro.php
+++ application/views/relatorios/imprimir/imprimirFinanceiro.php
@@ -49,7 +49,7 @@
                                     $saldo = 0;
                                     foreach ($lancamentos as $l) {
                                         $vencimento = date('d/m/Y', strtotime($l->data_vencimento));
-                                        $pagamento = date('d/m/Y', strtotime($l->data_pagamento));
+                                        $pagamento = $l->data_pagamento;
                                         if ($l->baixado == 1) {
                                             $situacao = 'Pago';
                                         } else {
@@ -60,6 +60,14 @@
                                         } else {
                                             $totalDespesa += $l->valor_desconto;
                                         }
+
+
+                                        if  ($pagamento == "0000-00-00")  {
+                                            $pagamento = "Não informado";
+                                        } else  {
+                                            $pagamento = date('d/m/Y', strtotime($pagamento));
+                                     }
+             
                                         echo '<tr>';
                                         echo '<td>' . $l->cliente_fornecedor . '</td>';
                                         echo '<td>' . $l->tipo . '</td>';
