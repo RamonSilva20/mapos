5c0eae5f70a83100e21fbf31178d63b501dd9fe4
Luiz Fernando Riva Nekel <luizfernandorivanekel@hotmail.com>
2022-04-29 20:06:46 -0400

    Update imprimirFinanceiro.php

--- application/views/relatorios/imprimir/imprimirFinanceiro.php
+++ application/views/relatorios/imprimir/imprimirFinanceiro.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>MAPOS</title>
     <meta charset="UTF-8" />
@@ -11,16 +12,22 @@
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/blue.css" class="skin-color" />
 </head>
+
 <body style="background-color: transparent">
+
     <div class="container-fluid">
+
         <div class="row-fluid">
             <div class="span12">
+
                 <div class="widget-box">
                     <?= $topo ?>
+
                     <div class="widget-title">
                         <h4 style="text-align: center">Relatório Financeiro</h4>
                     </div>
                     <div class="widget-content nopadding tab-content">
+
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
@@ -36,41 +43,36 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                
                                 <?php
-                                $totalReceita = 0;
-                                $totalDespesa = 0;
-                                $saldo = 0;
-                                foreach ($lancamentos as $l) {
-                                    $vencimento = date('d/m/Y', strtotime($l->data_vencimento));
-                                    if ($l->baixado == 1) {
-                                        $situacao = 'Pago';
-                                    } else {
-                                        $situacao = 'Pendente';
-                                    }
-                                    if ($l->tipo == 'receita') {
-                                        $totalReceita += $l->valor_desconto;
-                                    } else {
-                                        $totalDespesa += $l->valor_desconto;
-                                    }
-                                    if ($l->data_pagamento == "0000-00-00") {
-                                        $pagamento = "Não informado";
-                                    } else {
+                                    $totalReceita = 0;
+                                    $totalDespesa = 0;
+                                    $saldo = 0;
+                                    foreach ($lancamentos as $l) {
+                                        $vencimento = date('d/m/Y', strtotime($l->data_vencimento));
                                         $pagamento = date('d/m/Y', strtotime($l->data_pagamento));
+                                        if ($l->baixado == 1) {
+                                            $situacao = 'Pago';
+                                        } else {
+                                            $situacao = 'Pendente';
+                                        }
+                                        if ($l->tipo == 'receita') {
+                                            $totalReceita += $l->valor_desconto;
+                                        } else {
+                                            $totalDespesa += $l->valor_desconto;
+                                        }
+                                        echo '<tr>';
+                                        echo '<td>' . $l->cliente_fornecedor . '</td>';
+                                        echo '<td>' . $l->tipo . '</td>';
+                                        echo '<td>' . 'R$ ' . number_format($l->valor, 2, ',', '.') . '</td>';
+                                        echo '<td>' . 'R$ ' . number_format($l->desconto, 2, ',', '.') . '</td>';
+                                        echo '<td>' . 'R$ ' . number_format($l->valor_desconto, 2, ',', '.') . '</td>';
+                                        echo '<td>' . $vencimento . '</td>';
+                                        echo '<td>' . $pagamento . '</td>';
+                                        echo '<td>' . $l->forma_pgto . '</td>';
+                                        echo '<td>' . $situacao . '</td>';
+                                        echo '</tr>';
                                     }
-                                    echo '<tr>';
-                                    echo '<td>' . $l->cliente_fornecedor . '</td>';
-                                    echo '<td>' . $l->tipo . '</td>';
-                                    echo '<td>' . 'R$ ' . number_format($l->valor, 2, ',', '.') . '</td>';
-                                    echo '<td>' . 'R$ ' . number_format($l->desconto, 2, ',', '.') . '</td>';
-                                    echo '<td>' . 'R$ ' . number_format($l->valor_desconto, 2, ',', '.') . '</td>';
-                                    echo '<td>' . $vencimento . '</td>';
-                                    echo '<td>' . $pagamento . '</td>';
-                                    echo '<td>' . $l->forma_pgto . '</td>';
-                                    echo '<td>' . $situacao . '</td>';
-                                    echo '</tr>';
-                                }
-                                ?>
+                                    ?>
                             </tbody>
                             <tfoot>
                                 <tr>
@@ -110,8 +112,10 @@
                 <h5 style="text-align: right">Data do Relatório:
                     <?php echo date('d/m/Y'); ?>
                 </h5>
+
             </div>
         </div>
     </div>
 </body>
+
 </html>
