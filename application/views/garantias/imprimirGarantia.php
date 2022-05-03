e6e40856e3cdff438ff5ed0c600742dc415d9558
Eliabe Clayton <53370621+eliabeclayton@users.noreply.github.com>
2022-05-03 20:17:32 -0300

    Correção - Navegador bloqueando o Google Fonts
    Navegador bloqueando o google fonts por estar com endereço de navegação insegura (http) ao invés de (https).
    
    Alteração: 
    de   ---> http://fonts.googleapis.com/css?family=Open+Sans:400,700,800
    para ---> https://fonts.googleapis.com/css?family=Open+Sans:400,700,800
    
    Alteração feita em todos os arquivos mencionados abaixo:
    mapos\application\views\conecte\imprimirOs.php
    mapos\application\views\conecte\imprimirVenda.php
    mapos\application\views\garantias\imprimirGarantia.php
    mapos\application\views\os\imprimirOs.php
    mapos\application\views\os\imprimirOsTermica.php
    mapos\application\views\tema\topo.php
    mapos\application\views\vendas\imprimirVenda.php
    mapos\application\views\vendas\imprimirVendaTermica.php

--- application/views/garantias/imprimirGarantia.php
+++ application/views/garantias/imprimirGarantia.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/matrix-style.css" />
     <link href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
     <link href="<?= base_url('assets/css/custom.css'); ?>" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
     <style>
         .table {
             margin-bottom: 5px;
