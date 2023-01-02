a16dc4dacc3ee4f6448af378209aee6685d955de
Cleyton TI <cleytonandrade@gmail.com>
2023-01-02 09:53:22 -0300

    Update imprimirVendas.php
    Adicionado tag Title com variável app_nome

--- application/views/relatorios/imprimir/imprimirVendas.php
+++ application/views/relatorios/imprimir/imprimirVendas.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title><?= $this->config>->item('app_name') ?> </title>
+    <title><?= $this->config>->item('app_name') ?></title>
     <meta charset="UTF-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
