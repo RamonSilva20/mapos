44be6345acffb3b764b362e492ec27e63315faef
Cleyton TI <cleytonandrade@gmail.com>
2023-01-02 09:34:54 -0300

    Update imprimirServicos.php
    Adicionado tag Title com varivável app_nome

--- application/views/relatorios/imprimir/imprimirServicos.php
+++ application/views/relatorios/imprimir/imprimirServicos.php
@@ -2,10 +2,8 @@
 <html>
 
 <head>
-    <title>MAPOS</title>
-    <meta charset="UTF-8" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= $this->config->item('app_name') ?> </title>
+    <meta charset="UTF-8" /><title><?= $this->config->item('app_name') ?> </title>tial-scale=1.0" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.min.css" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/fullcalendar.css" />
