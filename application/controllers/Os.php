07e51ebf2886aac802d9e42d8d8ff3417f8591b7
willph <willph@users.noreply.github.com>
2023-11-04 22:42:06 +0000

    Apply php-cs-fixer changes

--- application/controllers/Os.php
+++ application/controllers/Os.php
@@ -1024,15 +1024,15 @@ class Os extends MY_Controller
         foreach ($remetentes as $remetente) {
             if ($remetente) {
                 $headers = ['From' => $emitente->email, 'Subject' => $assunto, 'Return-Path' => ''];
-            $email = [
-                'to' => $remetente,
-                'message' => $html,
-                'status' => 'pending',
-                'date' => date('Y-m-d H:i:s'),
-                'headers' => serialize($headers),
-            ];
-            $this->email_model->add('email_queue', $email);
-            }else{
+                $email = [
+                    'to' => $remetente,
+                    'message' => $html,
+                    'status' => 'pending',
+                    'date' => date('Y-m-d H:i:s'),
+                    'headers' => serialize($headers),
+                ];
+                $this->email_model->add('email_queue', $email);
+            } else {
                 log_info('Email não adicionado a Lista de envio de e-mails. Verifique se o remetente esta cadastrado. OS ID: ' . $idOs);
             }
         }
