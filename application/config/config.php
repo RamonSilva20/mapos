80fa601445cb321ea5857c11b099a36d62b6c802
Felipe Santt <33626716+Fesantt@users.noreply.github.com>
2024-02-10 00:01:36 -0300

    Update config.php
    cookie :D

--- application/config/config.php
+++ application/config/config.php
@@ -443,7 +443,7 @@ $config['cookie_httponly'] 	= false;
 */
 $config['csrf_protection'] = true;
 $config['csrf_token_name'] = 'MAPOS_TOKEN';
-$config['csrf_cookie_name'] = 'MAPOS_COKIE';
+$config['csrf_cookie_name'] = 'MAPOS_COOKIE';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = true;
 $config['csrf_exclude_uris'] = [
