b3699e3878a3cf6937f8d1fbd23a563ba048bfef
Julio Lobo <juliolobo@lobohost.com.br>
2024-02-22 17:17:27 -0300

    Ajustes do Github_updater.php e captcha (#2339)
    * Ajuste do Github_updater.php e captcha
    Removendo a criação de diretório do Github_updater
    Refeito geraçãod e captcha
    Realocando arquivos de fonte
    
    * Atualizar o cadastrar.php

--- application/libraries/Github_updater.php
+++ application/libraries/Github_updater.php
@@ -91,19 +91,7 @@ class Github_updater
                             }
                         }
                         // Otherwise copy the file from the update.
-                        else {
-                            $arrFilename = explode('/', $file->filename);
-                            if(is_array($arrFilename) && count($arrFilename) > 0) {
-                                $diretorio = '';
-                                for ($i = 0; $i < (count($arrFilename) - 1); $i++) {
-                                    $diretorio .= $arrFilename[$i].'/';
-                                }
-                                
-                                if(!file_exists($diretorio)) {
-                                    mkdir($diretorio, 0755, true);
-                                }
-                            }
-                            
+                        else {                            
                             copy($dir.'/'.$file->filename, $file->filename);
                         }
                     }
