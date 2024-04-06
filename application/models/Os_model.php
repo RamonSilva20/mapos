41d0a546581fb4c98c61ade34458877124ff1ad1
cabralwms <61280919+cabralwms@users.noreply.github.com>
2024-04-06 11:37:31 -0300

    Corrigindo buscas por CPF, autocomplete com CPF

--- application/models/Os_model.php
+++ application/models/Os_model.php
@@ -40,6 +40,7 @@ class Os_model extends CI_Model
             if (array_key_exists('pesquisa', $where)) {
                 $this->db->select('idClientes');
                 $this->db->like('nomeCliente', $where['pesquisa']);
+                $this->db->like('documento', $where['pesquisa']);
                 $this->db->limit(25);
                 $clientes = $this->db->get('clientes')->result();
 
@@ -217,10 +218,11 @@ class Os_model extends CI_Model
         $this->db->like('nomeCliente', $q);
         $this->db->or_like('telefone', $q);
         $this->db->or_like('celular', $q);
+        $this->db->or_like('documento', $q);
         $query = $this->db->get('clientes');
         if ($query->num_rows() > 0) {
             foreach ($query->result_array() as $row) {
-                $row_set[] = ['label' => $row['nomeCliente'] . ' | Telefone: ' . $row['telefone'] . ' | Celular: ' . $row['celular'], 'id' => $row['idClientes']];
+                $row_set[] = ['label' => $row['nomeCliente'] . ' | Telefone: ' . $row['telefone'] . ' | Celular: ' . $row['celular'] . ' | Documento: ' . $row['documento'], 'id' => $row['idClientes']];
             }
             echo json_encode($row_set);
         }
