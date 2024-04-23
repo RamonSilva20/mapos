2981727c87a02e1e3654b29a06793091aff5c048
Gianluca Bine <gian_bine@hotmail.com>
2024-04-23 00:35:29 -0300

    fix: movido pastas de API

--- application/config/routes_api.php
+++ application/config/routes_api.php
@@ -4,30 +4,30 @@ if (! defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-// Rotas API
-$route['api'] = 'api/ApiController/index';
-$route['api/audit'] = 'api/ApiController/audit';
-$route['api/emitente'] = 'api/ApiController/emitente';
-$route['api/calendario'] = 'api/ApiController/calendario';
-$route['api/login'] = 'api/UsuariosController/login';
-$route['api/reGenToken'] = 'api/UsuariosController/reGenToken';
-$route['api/conta'] = 'api/UsuariosController/conta';
-$route['api/clientes'] = 'api/ClientesController/index';
-$route['api/clientes/(:num)'] = 'api/ClientesController/index/$1';
-$route['api/produtos'] = 'api/ProdutosController/index';
-$route['api/produtos/(:num)'] = 'api/ProdutosController/index/$1';
-$route['api/servicos'] = 'api/ServicosController/index';
-$route['api/servicos/(:num)'] = 'api/ServicosController/index/$1';
-$route['api/usuarios'] = 'api/UsuariosController/index';
-$route['api/usuarios/(:num)'] = 'api/UsuariosController/index/$1';
-$route['api/os'] = 'api/OsController/index';
-$route['api/os/(:num)'] = 'api/OsController/index/$1';
-$route['api/os/(:num)/produtos'] = 'api/OsController/produtos/$1';
-$route['api/os/(:num)/produtos/(:num)'] = 'api/OsController/produtos/$1/$2';
-$route['api/os/(:num)/servicos'] = 'api/OsController/servicos/$1';
-$route['api/os/(:num)/servicos/(:num)'] = 'api/OsController/servicos/$1/$2';
-$route['api/os/(:num)/anotacoes'] = 'api/OsController/anotacoes/$1';
-$route['api/os/(:num)/anotacoes/(:num)'] = 'api/OsController/anotacoes/$1/$2';
-$route['api/os/(:num)/anexos'] = 'api/OsController/anexos/$1';
-$route['api/os/(:num)/anexos/(:num)'] = 'api/OsController/anexos/$1/$2';
-$route['api/os/(:num)/desconto'] = 'api/OsController/desconto/$1';
+// Rotas API V1
+$route['api/v1'] = 'api/v1/ApiController/index';
+$route['api/v1/audit'] = 'api/v1/ApiController/audit';
+$route['api/v1/emitente'] = 'api/v1/ApiController/emitente';
+$route['api/v1/calendario'] = 'api/v1/ApiController/calendario';
+$route['api/v1/login'] = 'api/v1/UsuariosController/login';
+$route['api/v1/reGenToken'] = 'api/v1/UsuariosController/reGenToken';
+$route['api/v1/conta'] = 'api/v1/UsuariosController/conta';
+$route['api/v1/clientes'] = 'api/v1/ClientesController/index';
+$route['api/v1/clientes/(:num)'] = 'api/v1/ClientesController/index/$1';
+$route['api/v1/produtos'] = 'api/v1/ProdutosController/index';
+$route['api/v1/produtos/(:num)'] = 'api/v1/ProdutosController/index/$1';
+$route['api/v1/servicos'] = 'api/v1/ServicosController/index';
+$route['api/v1/servicos/(:num)'] = 'api/v1/ServicosController/index/$1';
+$route['api/v1/usuarios'] = 'api/v1/UsuariosController/index';
+$route['api/v1/usuarios/(:num)'] = 'api/v1/UsuariosController/index/$1';
+$route['api/v1/os'] = 'api/v1/OsController/index';
+$route['api/v1/os/(:num)'] = 'api/v1/OsController/index/$1';
+$route['api/v1/os/(:num)/produtos'] = 'api/v1/OsController/produtos/$1';
+$route['api/v1/os/(:num)/produtos/(:num)'] = 'api/v1/OsController/produtos/$1/$2';
+$route['api/v1/os/(:num)/servicos'] = 'api/v1/OsController/servicos/$1';
+$route['api/v1/os/(:num)/servicos/(:num)'] = 'api/v1/OsController/servicos/$1/$2';
+$route['api/v1/os/(:num)/anotacoes'] = 'api/v1/OsController/anotacoes/$1';
+$route['api/v1/os/(:num)/anotacoes/(:num)'] = 'api/v1/OsController/anotacoes/$1/$2';
+$route['api/v1/os/(:num)/anexos'] = 'api/v1/OsController/anexos/$1';
+$route['api/v1/os/(:num)/anexos/(:num)'] = 'api/v1/OsController/anexos/$1/$2';
+$route['api/v1/os/(:num)/desconto'] = 'api/v1/OsController/desconto/$1';
