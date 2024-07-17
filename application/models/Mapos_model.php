e615d856142006f993eb2e5c5a4ad767ecbd61ff
cabralwms <cabralfw@hotmail.com>
2024-07-17 15:00:08 -0300

    Melhorada as informações do Painel

--- application/models/Mapos_model.php
+++ application/models/Mapos_model.php
@@ -116,13 +116,47 @@ class Mapos_model extends CI_Model
         return $this->db->count_all($table);
     }
 
+    public function getOsOrcamentos()
+    {
+        $this->db->select('os.*, clientes.nomeCliente');
+        $this->db->from('os');
+        $this->db->join('clientes', 'clientes.idClientes = os.clientes_id');
+        $this->db->where('os.status', 'Orçamento');
+        $this->db->limit(5);
+
+        return $this->db->get()->result();
+    }
+    
     public function getOsAbertas()
     {
         $this->db->select('os.*, clientes.nomeCliente');
         $this->db->from('os');
         $this->db->join('clientes', 'clientes.idClientes = os.clientes_id');
         $this->db->where('os.status', 'Aberto');
-        $this->db->limit(10);
+        $this->db->limit(5);
+
+        return $this->db->get()->result();
+    }
+
+    public function getOsFinalizadas()
+    {
+        $this->db->select('os.*, clientes.nomeCliente');
+        $this->db->from('os');
+        $this->db->join('clientes', 'clientes.idClientes = os.clientes_id');
+        $this->db->where('os.status', 'Finalizado');
+        $this->db->order_by('os.idOs', 'DESC');
+        $this->db->limit(5);
+
+        return $this->db->get()->result();
+    }
+
+    public function getOsAprovadas()
+    {
+        $this->db->select('os.*, clientes.nomeCliente');
+        $this->db->from('os');
+        $this->db->join('clientes', 'clientes.idClientes = os.clientes_id');
+        $this->db->where('os.status', 'Aprovado');
+        $this->db->limit(5);
 
         return $this->db->get()->result();
     }
@@ -133,7 +167,7 @@ class Mapos_model extends CI_Model
         $this->db->from('os');
         $this->db->join('clientes', 'clientes.idClientes = os.clientes_id');
         $this->db->where('os.status', 'Aguardando Peças');
-        $this->db->limit(10);
+        $this->db->limit(5);
 
         return $this->db->get()->result();
     }
@@ -144,9 +178,45 @@ class Mapos_model extends CI_Model
         $this->db->from('os');
         $this->db->join('clientes', 'clientes.idClientes = os.clientes_id');
         $this->db->where('os.status', 'Em Andamento');
-        $this->db->limit(10);
+        $this->db->limit(5);
 
         return $this->db->get()->result();
+    }
+
+    public function getOsStatus($status)
+    {
+        $this->db->select('os.*, clientes.nomeCliente');
+        $this->db->from('os');
+        $this->db->join('clientes', 'clientes.idClientes = os.clientes_id');
+        $this->db->where_in('os.status', $status);
+        $this->db->order_by('os.idOs', 'DESC');
+        $this->db->limit(5);
+
+        return $this->db->get()->result();
+    }
+    
+    public function getVendasStatus($vstatus)
+    {
+        $this->db->select('vendas.*, clientes.nomeCliente');
+        $this->db->from('vendas');
+        $this->db->join('clientes', 'clientes.idClientes = vendas.clientes_id');
+        $this->db->where_in('vendas.status', $vstatus);
+        $this->db->order_by('vendas.idVendas', 'DESC');
+        $this->db->limit(5);
+
+        return $this->db->get()->result();
+    }
+
+    public function getLancamentos()
+    {
+        $this->db->select('idLancamentos, tipo, cliente_fornecedor, descricao, data_vencimento, forma_pgto, valor_desconto, baixado');
+        $this->db->from('lancamentos');
+        $this->db->where('baixado', 0);
+        $this->db->order_by('idLancamentos', 'DESC');
+        $this->db->limit(10);
+
+        $query = $this->db->get();
+        return $query->result();
     }
 
     public function calendario($start, $end, $status = null)
