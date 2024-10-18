2b127a2d1730f9c7e3b4b474f02a806823445895
Apoio Nacional <108497625+apoionacional@users.noreply.github.com>
2024-10-18 14:35:19 -0300

    Update funcoesGlobal.js
    Remoção da função de fechamento automático padrão das div.alert.

--- assets/js/funcoesGlobal.js
+++ assets/js/funcoesGlobal.js
@@ -1,9 +1,3 @@
-$().ready(function () {
-    setTimeout(function () {
-        $('div.alert').delay(1500).fadeOut(400); // "div.alert" é a div que tem a class alert do elemento que deseja manipular.
-    }, 2500); // O valor é representado em milisegundos.
-});
-
 // Removendo o atributo tittle para dispositivos moveis.
 $(document).ready(function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
