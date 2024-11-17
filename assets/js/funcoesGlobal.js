b6619f13076e2b647cf67437597b2576592d346a
Wilmerson <will.phelipe@gmail.com>
2024-11-17 17:06:54 -0300

    Revert "Correções e melhorias em servicos.php, clientes.php, produtos.php (#2…"
    This reverts commit dc9e769f87ae4d46840766a7bb78ba218fde000f.

--- assets/js/funcoesGlobal.js
+++ assets/js/funcoesGlobal.js
@@ -1,23 +1,31 @@
+$().ready(function () {
+    setTimeout(function () {
+        $('div.alert').delay(1500).fadeOut(400); // "div.alert" é a div que tem a class alert do elemento que deseja manipular.
+    }, 2500); // O valor é representado em milisegundos.
+});
+
+// Removendo o atributo tittle para dispositivos moveis.
 $(document).ready(function () {
-    const isMobile = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    if (isMobile) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         $(".tip-top").removeAttr("title");
     }
-    initTimer();
 });
 
 function showTimer() {
-    const timerElement = document.getElementById("timer");
-    if (timerElement) {
-        const time = new Date();
-        const hour = String(time.getHours()).padStart(2, '0');
-        const minute = String(time.getMinutes()).padStart(2, '0');
-        const second = String(time.getSeconds()).padStart(2, '0');
+    var time = new Date();
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var second = time.getSeconds();
 
-        timerElement.innerHTML = `${hour}:${minute}:${second}`;
-    }
+    if (hour < 10) hour = "0" + hour;
+    if (minute < 10) minute = "0" + minute;
+    if (second < 10) second = "0" + second;
+
+    var st = hour + ":" + minute + ":" + second; document.getElementById("timer").innerHTML = st;
 }
 
 function initTimer() {
+
+    // O metodo nativo setInterval executa uma determinada funcao em um determinado tempo
     setInterval(showTimer, 1000);
 }
