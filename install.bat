dbfc9e625261f0a6f07bb5289d80bb47f98fe0b6
Barreto <45976190+barretowiisk@users.noreply.github.com>
2025-03-06 21:14:08 -0300

    Update install.bat
    Correção textual.
    
    Co-authored-by: Gianluca Bine <gian_bine@hotmail.com>

--- install.bat
+++ install.bat
@@ -149,7 +149,7 @@ GOTO etapa0
 :etapa4
 ECHO # BAIXANDO DEPENDENCIAS...
 ECHO.
-ECHO - Criando diretortio de instalacao
+ECHO - Criando diretório de instalacao
 IF not EXIST %dirDefault% mkdir %dirDefault% >NUL 2>&1
 ECHO - Verificando Wget
 IF not EXIST "%dirDefault%\wget.exe" PowerShell -command "& { iwr %urlWget% -OutFile %dirDefault%\wget.exe }" >NUL 2>&1
