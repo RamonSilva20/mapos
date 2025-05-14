62db7667cb076f7e092141297253a905c2b46697
Asker <33626716+Fesantt@users.noreply.github.com>
2025-05-14 19:39:58 -0300

    Update application/controllers/Vendas.php
    Co-authored-by: Gianluca Bine <gian_bine@hotmail.com>

--- application/controllers/Vendas.php
+++ application/controllers/Vendas.php
@@ -122,7 +122,6 @@ class Vendas extends MY_Controller
 
     public function editar()
     {
-
         if (! $this->uri->segment(3) || ! is_numeric($this->uri->segment(3))) {
             $this->session->set_flashdata('error', 'Item não pode ser encontrado, parâmetro não foi passado corretamente.');
             redirect('mapos');
