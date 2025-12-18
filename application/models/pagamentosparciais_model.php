23a1e5441fe61fe53f21433d8a34ff9e007dec44
marques823 <marques823@gmail.com>
2025-12-17 23:49:16 -0300

    fix: Corrige nome da classe do model para corresponder ao nome do arquivo
    - Altera PagamentosParciais_model para Pagamentosparciais_model
    - CodeIgniter requer que nome do arquivo e classe correspondam exatamente

--- application/models/pagamentosparciais_model.php
+++ application/models/pagamentosparciais_model.php
@@ -7,7 +7,7 @@ if (! defined('BASEPATH')) {
 /**
  * Model para gerenciamento de pagamentos parciais
  */
-class PagamentosParciais_model extends CI_Model
+class Pagamentosparciais_model extends CI_Model
 {
     public function __construct()
     {
