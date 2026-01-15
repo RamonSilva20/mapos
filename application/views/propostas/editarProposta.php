f4fc8d95af456e1d9345889e754facd94f6229e1
marques823 <marques823@gmail.com>
2026-01-14 23:59:16 -0300

    fix: Remove chamada restante de função removida
    - Remove chamada de inicializarAutocompletes() no setTimeout
    - Autocompletes agora são inicializados diretamente no document.ready
    - Formato idêntico ao editarOs que funciona corretamente

--- application/views/propostas/editarProposta.php
+++ application/views/propostas/editarProposta.php
@@ -488,9 +488,6 @@ $(document).ready(function() {
 
     // Aguardar um pouco para garantir que todos os elementos estejam prontos
     setTimeout(function() {
-        // Reinicializar autocompletes caso necessário
-        inicializarAutocompletes();
-        
         // Carregar dados existentes
         if (produtos.length > 0) {
             atualizarTabelaProdutos();
